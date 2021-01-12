c23591abb98c67464c3ef3bac610e841734f9a7c
Hasib Al Rashid <69360193+Cadet-Hasib@users.noreply.github.com>
2021-01-12 21:56:18 +0600

    init

--- bot.js
+++ bot.js
@@ -175,6 +175,120 @@ bot.on("message", async (message) => {
         }
     }
 
+    if (command === "giveaway") {
+        var time = "";
+        var time2 = "";
+        var time3 = "";
+        if (!message.member.hasPermission("MANAGE_CHANNELS"))
+            return message.channel.send(
+                "You don't have enough permissions to use this command."
+            );
+        if (command === `giveaway`)
+            return message.channel.send(
+                `You didn\'t state a duration or a price for the giveaway.`
+            );
+        if (command !== `giveaway`) {
+            const stated_duration_hours = message.content.split(" ")[1];
+            const stated_duration_hours2 = stated_duration_hours.toLowerCase();
+            if (stated_duration_hours2.includes("s")) {
+                var time = "s";
+            }
+            if (stated_duration_hours2.includes("m")) {
+                var time = "m";
+            }
+            if (stated_duration_hours2.includes("h")) {
+                var time = "h";
+            }
+            if (stated_duration_hours2.includes("d")) {
+                var time = "d";
+            }
+            const stated_duration_hours3 = stated_duration_hours2.replace(
+                time,
+                ""
+            );
+            if (stated_duration_hours3 === "0") {
+                message.channel.send("The duration has to be atleast one.");
+            }
+            if (isNaN(stated_duration_hours3)) {
+                message.channel.send(
+                    "The duration has to be a valid time variable."
+                );
+            }
+            if (stated_duration_hours3 > 1) {
+                var time3 = "s";
+            }
+            if (time === "s") {
+                var actual_duration_hours = stated_duration_hours3 * 1000;
+                var time2 = "second";
+            }
+            if (time === "m") {
+                var actual_duration_hours = stated_duration_hours3 * 60000;
+                var time2 = "minute";
+            }
+            if (time === "h") {
+                var actual_duration_hours = stated_duration_hours3 * 3600000;
+                var time2 = "hour";
+            }
+            if (time === "d") {
+                var actual_duration_hours = stated_duration_hours3 * 86400000;
+                var time2 = "day";
+            }
+            if (!isNaN(stated_duration_hours3)) {
+                const prize = message.content.split(" ").slice(2).join(" ");
+                if (prize === "")
+                    return message.channel.send("You have to enter a price.");
+                if (stated_duration_hours3 !== "0") {
+                    const embed = new Discord.RichEmbed()
+                        .setTitle(`${prize}`)
+                        .setColor("36393F")
+                        .setDescription(
+                            `React with 🎉 to enter!\nTime duration: **${stated_duration_hours3}** ${time2}${time3}\nHosted by: ${message.author}`
+                        )
+                        .setTimestamp(Date.now() + actual_duration_hours)
+                        .setFooter("Ends at");
+                    let msg = await message.channel.send(
+                        ":tada: **GIVEAWAY** :tada:",
+                        embed
+                    );
+                    await msg.react("🎉");
+                    setTimeout(() => {
+                        setTimeout(() => {
+                            let winner = msg.reactions.get("🎉").users.random();
+                            if (msg.reactions.get("🎉").users.size < 1) {
+                                const winner_embed = new Discord.MessageEmbed()
+                                    .setTitle(`${prize}`)
+                                    .setColor("36393F")
+                                    .setDescription(
+                                        `Winner:\nNo one entered the giveaway.\nHosted by: ${message.author}`
+                                    )
+                                    .setTimestamp()
+                                    .setFooter("Ended at");
+                                msg.edit(
+                                    ":tada: **GIVEAWAY ENDED** :tada:",
+                                    winner_embed
+                                );
+                            }
+                            if (!msg.reactions.get("🎉").users.size < 1) {
+                                const winner_embed = new Discord.RichEmbed()
+                                    .setTitle(`${prize}`)
+                                    .setColor("36393F")
+                                    .setDescription(
+                                        `Winner:\n${winner}\nHosted by: ${message.author}`
+                                    )
+                                    .setTimestamp()
+                                    .setFooter("Ended at");
+                                msg.edit(
+                                    ":tada: **GIVEAWAY ENDED** :tada:",
+                                    winner_embed
+                                );
+                            }
+                        }, 1000);
+                    }, actual_duration_hours);
+                }
+            }
+        }
+    }
+
     if (command === "kick") {
         if (!message.member.hasPermission("KICK_MEMBERS"))
             return message.channel.send(":no_entry: Insufficient permissions");
@@ -250,22 +364,6 @@ bot.on("message", async (message) => {
                 });
             }
         }
-    }
-
-    if (command === "math-sqrt") {
-        const args = message.content.split(" ").slice(1);
-        const result = Math.sqrt(args);
-
-        message.channel.send(result);
-    }
-
-    if (command === "math-sum") {
-        const args1 = message.content.split(" ").slice(1);
-        const args2 = message.content.split(" ").slice(2);
-
-        const result = args1 + args2;
-
-        message.channel.send(result);
     }
 
     if (command === "add") {
