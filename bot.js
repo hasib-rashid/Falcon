7beccc91d16d9a735961c975aa145c4a0eac8039
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-01-26 23:06:22 +0600

    Adding adding Roles Functionality!

--- bot.js
+++ bot.js
@@ -266,7 +266,7 @@ bot.on("message", async (message) => {
                 if (stated_duration_hours3 !== "0") {
                     const embed = new Discord.MessageEmbed()
                         .setTitle(`${prize}`)
-                        .setColor("36393F")
+                        .setColor("RANDOM")
                         .setDescription(
                             `React with 🎉 to enter!\nTime duration: **${stated_duration_hours3}** ${time2}${time3}\nHosted by: ${message.author}`
                         )
@@ -394,6 +394,48 @@ bot.on("message", async (message) => {
                     message.channel.send(banned_embed);
                 });
             }
+        }
+    }
+
+    if (command === "add") {
+        if (message.member.hasPermission("MANAGE_EMOJIS")) {
+            const targetUser = message.mentions.users.first();
+
+            if (!targetUser) {
+                message.channel.send(":no_entry: Please specify a user first!");
+                return;
+            }
+
+            args.shift();
+
+            const roleName = args.join(" ");
+            const { guild } = message;
+
+            const role = guild.roles.cache.find((role) => {
+                return role.name === roleName;
+            });
+
+            if (!role) {
+                message.channel.send(
+                    `:no_entry: There is no role named as "${roleName}"`
+                );
+                return;
+            }
+
+            const memberUser = guild.members.cache.get(targetUser.id);
+            memberUser.roles.add(role);
+
+            const addRoleEmbed = new Discord.MessageEmbed()
+                .setTitle("Role Added! :thumbsup:")
+                .setColor("RANDOM")
+                .setDescription(
+                    `:thumbsup: ${role} was Successfully added to ${targetUser} by ${message.author.tag}`
+                )
+                .setFooter("Copyright @2021 CodeVert");
+
+            message.channel.send(addRoleEmbed);
+        } else {
+            message.channel.send(":no_entry: Insufficient Permissions");
         }
     }
 
