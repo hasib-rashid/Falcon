43861cbe78599bc26d1cb7e78dc9e62ef9c5a2d1
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-02-17 15:30:08 +0600

    😁Reaction Roles Complete

--- commands/admin/language-reaction-roles.js
+++ commands/admin/language-reaction-roles.js
@@ -2,19 +2,18 @@ const Discord = require("discord.js");
 const commando = require("discord.js-commando");
 const oneLine = require("common-tags").oneLine;
 
-module.exports = class ReactionRoleCommand extends commando.Command {
+module.exports = class LanguageReactionRolesCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: "rr",
+            name: "rr-lang",
             aliases: [],
             group: "moderation",
-            memberName: "rr",
-            description: "React to get your roles",
+            memberName: "rr-lang",
+            description: "Get the Language Reaction Roles in one Command",
             details: oneLine`
-                React to get your roles
+                Get the Language Reaction Roles in one Command
             `,
-            examples: ["!rr"],
-            clientPermissions: ["MANAGE_ROLES"],
+            examples: ["!rr-lang"],
         });
     }
 
@@ -25,14 +24,17 @@ module.exports = class ReactionRoleCommand extends commando.Command {
     async run(message) {
         const channel = "810921429671477249";
 
-        const WebDeveloper = "🖥️";
-        const MobileDeveloper = "📱";
-        const AIDeveloper = "🤖";
-        const BotDeveloper = "✨";
-        const PythonDeveloper = "🐍";
-        const LibraryDeveloper = "📦";
-        const APIDeveloper = "📰";
-        const GameDeveloper = "🎮";
+        const HTML = "811466149782224958";
+        const CSS = "811470391842963466";
+        const Javascript = "811466518280798278";
+        const NodeJS = "811465048882806824";
+        const Python = "811468429184991272";
+        const PHP = "811485994607968256";
+        const Dart = "811524799792152586";
+        const Swift = "811525276172419113";
+        const Java = "811485676364759070";
+        const CPP = "811471142325059614";
+        const C = "811471447083319376";
 
         let embed = new Discord.MessageEmbed()
             .setAuthor(message.guild.name, message.guild.iconURL())
@@ -40,28 +42,33 @@ module.exports = class ReactionRoleCommand extends commando.Command {
             .setTitle("Get your roles here!")
             .setDescription(
                 "**React to this message to get your roles! Programmer Based Roles**\n\n" +
-                    `${WebDeveloper} for Web Developer\n\n` +
-                    `${MobileDeveloper} for Mobile Developer\n\n` +
-                    `${PythonDeveloper} for Python Developer\n\n` +
-                    `${AIDeveloper} for AI Developer\n\n` +
-                    `${APIDeveloper} for API Developer\n\n` +
-                    `${LibraryDeveloper} for Library Developer\n\n` +
-                    `${BotDeveloper} for Bot Developer\n\n` +
-                    `${GameDeveloper} for Game Developer`
+                    `<:HTML:811466149782224958>  if you know HTML\n\n` +
+                    `<:CSS:811470391842963466>  if you know CSS\n\n` +
+                    `<:JavaScript:811466518280798278>  if you know Javascript\n\n` +
+                    `<:NodeJS:811465048882806824>  if you know NodeJS\n\n` +
+                    `<:Python:811468429184991272>  if you know Python\n\n` +
+                    `<:PHP:811485994607968256>  if you know PHP\n\n` +
+                    `<:Dart:811524799792152586>  if you know Dart\n\n` +
+                    `<:Swift:811525276172419113>  if you know Swift\n\n` +
+                    `<:Java:811485676364759070>  if you know Java\n\n` +
+                    `<:CPP:811471142325059614>  if you know C++\n\n` +
+                    `<:CLang:811471447083319376>  if you know C\n`
             )
             .setFooter("Reaction Roles in CodeVert");
 
         let messageEmbed = await message.channel.send(embed);
 
-        messageEmbed.react(WebDeveloper);
-        messageEmbed.react(MobileDeveloper);
-        messageEmbed.react(PythonDeveloper);
-        messageEmbed.react(AIDeveloper);
-        messageEmbed.react(APIDeveloper);
-        messageEmbed.react(LibraryDeveloper);
-        messageEmbed.react(BotDeveloper);
-        messageEmbed.react(AIDeveloper);
-        messageEmbed.react(GameDeveloper);
+        messageEmbed.react(HTML);
+        messageEmbed.react(CSS);
+        messageEmbed.react(Javascript);
+        messageEmbed.react(NodeJS);
+        messageEmbed.react(Python);
+        messageEmbed.react(PHP);
+        messageEmbed.react(Dart);
+        messageEmbed.react(Swift);
+        messageEmbed.react(Java);
+        messageEmbed.react(CPP);
+        messageEmbed.react(C);
 
         this.client.on("messageReactionAdd", async (reaction, user) => {
             try {
@@ -71,46 +78,52 @@ module.exports = class ReactionRoleCommand extends commando.Command {
                 if (!reaction.message.guild) return;
 
                 if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji.name === WebDeveloper) {
+                    if (reaction.emoji.name === HTML) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("784029110513958927");
+                            .roles.add("811503906274279434");
                     }
 
-                    if (reaction.emoji.name === MobileDeveloper) {
+                    if (reaction.emoji.name === CSS) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("811501292799262721");
+                            .roles.add("811504196393500705");
                     }
 
-                    if (reaction.emoji.name === PythonDeveloper) {
+                    if (reaction.emoji.name === Javascript) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("811501116583051274");
+                            .roles.add("811509151497977858");
                     }
 
-                    if (reaction.emoji.name === BotDeveloper) {
+                    if (reaction.emoji.name === NodeJS) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("784029110513958926");
+                            .roles.add("811509296176431137");
                     }
 
-                    if (reaction.emoji.name === LibraryDeveloper) {
+                    if (reaction.emoji.name === Python) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("784029110513958929");
+                            .roles.add("811509238152560660");
                     }
 
-                    if (reaction.emoji.name === AIDeveloper) {
+                    if (reaction.emoji.name === PHP) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("811497482835197953");
+                            .roles.add("811509213096181772");
                     }
 
-                    if (reaction.emoji.name === GameDeveloper) {
+                    if (reaction.emoji.name === Dart) {
                         await reaction.message.guild.members.cache
                             .get(user.id)
-                            .roles.add("784029110513958930");
+                            .roles.add("811509454108622858");
+                    }
+
+                    if (reaction.emoji.name === Swift) {
+                        await reaction.message.guild.members.cache
+                            .get(user.id)
+                            .roles.add("811509454159347772");
                     }
                 } else {
                     return;
