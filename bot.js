4c65c987a5debecb77527c9fd6df24303ae0bca5
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-02-20 11:40:21 +0600

    🎆Finally Finishing Reaction Roles

--- bot.js
+++ bot.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 
-const { CommandoClient, CommandoMessage } = require("discord.js-commando");
+const { CommandoClient } = require("discord.js-commando");
 const path = require("path");
 const Canvas = require("discord-canvas");
+const { ReactionRoleManager } = require("discord.js-collector");
+
 const client = new CommandoClient({
     commandPrefix: process.env.PREFIX,
     owner: "548038495617417226",
-    partials: ["MESSAGE", "REACTION", "CHANNEL", "USER"],
+    autoReconnect: true,
+    partials: [
+        "MESSAGE",
+        "CHANNEL",
+        "GUILD_MEMBER",
+        "REACTION",
+        "MESSAGE",
+        "USER",
+    ],
+});
+
+const reactionRoleManager = new ReactionRoleManager(client, {
+    mongoDbLink: process.env.MONGO_PATH, // See here to see how setup mongoose: https://github.com/IDjinn/Discord.js-Collector/blob/master/examples/reaction-role-manager/Note.md
 });
 
 client.registry
@@ -35,6 +49,79 @@ client.registry
 
 client.once("ready", () => {
     console.log(`[READY] Logged in as ${client.user.tag}!`);
+});
+
+// Triggered when the bot doesn't have permissions to manage this role.
+reactionRoleManager.on(
+    "missingPermissions",
+    (action, member, roles, reactionRole) => {
+        console.log(
+            `Some roles cannot be ${
+                action === 1 ? "given" : "taken"
+            } to member \`${
+                member.displayName
+            }\`, because i don't have permissions to manage these roles: ${roles
+                .map((role) => `\`${role.name}\``)
+                .join(",")}`
+        );
+    }
+);
+
+client.on("message", async (message) => {
+    const client = message.client;
+    const args = message.content.split(" ").slice(1);
+    // Example
+    // >createReactionRole @role :emoji: MessageId
+    if (message.content.startsWith("!createRR")) {
+        const role = message.mentions.roles.first();
+        if (!role)
+            return message
+                .reply("You need mention a role")
+                .then((m) => m.delete({ timeout: 1000 }));
+
+        const emoji = args[1];
+        if (!emoji)
+            return message
+                .reply("You need use a valid emoji.")
+                .then((m) => m.delete({ timeout: 1000 }));
+
+        const msg = await message.channel.messages.fetch(args[2] || message.id);
+        if (!role)
+            return message
+                .reply("Message not found! Wtf...")
+                .then((m) => m.delete({ timeout: 1000 }));
+
+        reactionRoleManager.createReactionRole({
+            message: msg,
+            roles: [role],
+            emoji,
+            type: 1,
+        });
+        /**
+         * Reaction Role Type
+         * NORMAL [1] - This role works like basic reaction role.
+         * TOGGLE [2] - You can win only one role of all toggle roles in this message (like colors system)
+         * JUST_WIN [3] - This role you'll only win, not lose.
+         * JUST_LOSE [4] - This role you'll only lose, not win.
+         * REVERSED [5] - This is reversed role. When react, you'll lose it, when you take off reaction you'll win it.
+         */
+
+        message.reply("Done").then((m) => m.delete({ timeout: 500 }));
+    } else if (message.content.startsWith("!deleteRR")) {
+        const emoji = args[0];
+        if (!emoji)
+            return message
+                .reply("You need use a valid emoji.")
+                .then((m) => m.delete({ timeout: 1000 }));
+
+        const msg = await message.channel.messages.fetch(args[1]);
+        if (!msg)
+            return message
+                .reply("Message not found! Wtf...")
+                .then((m) => m.delete({ timeout: 1000 }));
+
+        await reactionRoleManager.deleteReactionRole({ message: msg, emoji });
+    }
 });
 
 client.on("error", console.error);
--- commands/general/help.js
+++ commands/general/help.js
@@ -14,7 +14,7 @@ const {
     MISC,
     Fun,
     Search,
-} = require("../../Schema/JSON/help.json");
+} = require("../../JSON/help.json");
 
 module.exports = class HelpCommand extends commando.Command {
     constructor(client) {
