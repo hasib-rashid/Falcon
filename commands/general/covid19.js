403b9772dca175d9bc8c9b3ff545d2339db7a99d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-02-28 23:06:44 +0600

    🦠Virus Command Complete

--- commands/general/covid19.js
+++ commands/general/covid19.js
@@ -24,66 +24,142 @@ module.exports = class Covid19Command extends commando.Command {
      */
 
     async run(message) {
-        const args = message.content.split(" ").slice(1);
-        const { body } = await request.get(
-            "https://corona.lmao.ninja/v2/all?yesterday"
-        );
+        const query = message.content.split(" ").slice(1);
 
-        let embed = new Discord.MessageEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL())
-            .setTitle("Covid 19 Information")
-            .setDescription(
-                "View all the latest Covid 19 news in CodeVert. The situation of Covid is on the way."
-            )
-            .addFields(
-                {
-                    name: "❯ Total Cases",
-                    value: formatNumber(body.cases),
-                    inline: true,
-                },
-                {
-                    name: "❯ Total Deaths",
-                    value: formatNumber(body.deaths),
-                    inline: true,
-                },
-                {
-                    name: "❯ Total Recovery",
-                    value: formatNumber(body.recovered),
-                    inline: true,
-                },
-                {
-                    name: "❯ Cases Today",
-                    value: formatNumber(body.todayCases),
-                    inline: true,
-                },
-                {
-                    name: "❯ Deaths Today",
-                    value: formatNumber(body.todayDeaths),
-                    inline: true,
-                },
-                {
-                    name: "❯ Recovery Today",
-                    value: formatNumber(body.todayRecovered),
-                    inline: true,
-                },
-                {
-                    name: "❯ Active Cases",
-                    value: formatNumber(body.active),
-                    inline: true,
-                },
-                {
-                    name: "❯ Critical Cases",
-                    value: formatNumber(body.critical),
-                    inline: true,
-                },
-                {
-                    name: "❯ Tests Today",
-                    value: formatNumber(body.tests),
-                    inline: true,
-                }
-            )
-            .setColor("BLUE");
+        try {
+            if (!query) {
+                const { body } = await request.get(
+                    `https://corona.lmao.ninja/v2/countries/${query}?yesterday=true&strict=true&query`
+                );
 
-        message.channel.send(embed);
+                console.log(body);
+
+                let Main_embed = new Discord.MessageEmbed()
+                    .setAuthor(
+                        "Covid 19",
+                        "https://media.discordapp.net/attachments/793772583946027050/815533789761503232/image-removebg-preview_6.png?width=498&height=498"
+                    )
+                    .setTitle("Covid 19 Information")
+                    .setDescription(
+                        "View all the latest Covid 19 news in CodeVert. The situation of Covid is on the way."
+                    )
+                    .addFields(
+                        {
+                            name: "❯ Total Cases",
+                            value: formatNumber(body.cases),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Total Deaths",
+                            value: formatNumber(body.deaths),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Total Recovery",
+                            value: formatNumber(body.recovered),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Cases Today",
+                            value: formatNumber(body.todayCases),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Deaths Today",
+                            value: formatNumber(body.todayDeaths),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Recovery Today",
+                            value: formatNumber(body.todayRecovered),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Active Cases",
+                            value: formatNumber(body.active),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Critical Cases",
+                            value: formatNumber(body.critical),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Tests Today",
+                            value: formatNumber(body.tests),
+                            inline: true,
+                        }
+                    )
+                    .setColor("BLUE");
+
+                message.channel.send(Main_embed);
+            } else {
+                const { body } = await request.get(
+                    "https://corona.lmao.ninja/v2/all?yesterday"
+                );
+
+                let embed = new Discord.MessageEmbed()
+                    .setAuthor(
+                        message.author.username,
+                        message.author.avatarURL()
+                    )
+                    .setTitle("Covid 19 Information")
+                    .setDescription(
+                        "View all the latest Covid 19 news in CodeVert. The situation of Covid is on the way."
+                    )
+                    .addFields(
+                        {
+                            name: "❯ Total Cases",
+                            value: body.cases,
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Total Deaths",
+                            value: formatNumber(body.deaths),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Total Recovery",
+                            value: formatNumber(body.recovered),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Cases Today",
+                            value: formatNumber(body.todayCases),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Deaths Today",
+                            value: formatNumber(body.todayDeaths),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Recovery Today",
+                            value: formatNumber(body.todayRecovered),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Active Cases",
+                            value: formatNumber(body.active),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Critical Cases",
+                            value: formatNumber(body.critical),
+                            inline: true,
+                        },
+                        {
+                            name: "❯ Tests Today",
+                            value: formatNumber(body.tests),
+                            inline: true,
+                        }
+                    )
+                    .setColor("BLUE");
+
+                message.channel.send(embed);
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
 };
