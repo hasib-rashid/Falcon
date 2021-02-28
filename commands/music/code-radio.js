f7c3e116b88ae93716bf269aa03b6b8d8bd80001
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-02-28 23:43:58 +0600

    📻CodeRadio Stop

--- commands/music/code-radio.js
+++ commands/music/code-radio.js
@@ -24,54 +24,65 @@ module.exports = class CodeRadioCommand extends commando.Command {
      */
 
     async run(message) {
-        const cmd = message.content.split(" ").slice(1);
-        const voiceChannel = message.member.voice.channel;
+        try {
+            const cmd = message.content.split(" ").slice(1);
+            const voiceChannel = message.member.voice.channel;
 
-        if (!cmd) {
-            message.channel.send(
-                ":no_entry: Use a valid music command from the help menu."
-            );
-        }
-
-        if (cmd == "start") {
-            if (!voiceChannel) {
-                return message.channel.send(
-                    "You need to be in a voice channel!"
-                );
-            }
-
-            const permissions = voiceChannel.permissionsFor(
-                message.client.user
-            );
-
-            if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-                return message.channel.send(
-                    "I don't have permissions to join and speak in that voice channel!"
+            if (!cmd) {
+                message.channel.send(
+                    ":no_entry: Use a valid music command from the help menu."
                 );
             }
 
             let connectionDispatcher;
 
-            voiceChannel.join().then((connection) => {
-                connectionDispatcher = connection.play(
-                    "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3"
+            if (cmd == "start") {
+                if (!voiceChannel) {
+                    return message.channel.send(
+                        "You need to be in a voice channel!"
+                    );
+                }
+
+                const permissions = voiceChannel.permissionsFor(
+                    message.client.user
                 );
 
-                const radio_play_embed = new Discord.MessageEmbed()
-                    .setTitle("Started Playing the Radio!")
-                    .setDescription(
-                        "**:white_check_mark: Joined the Voice Channel and Playing the radio.** \n \n *Powered by FreeCodeCamp Radio*"
-                    )
-                    .setThumbnail(
-                        "https://media.discordapp.net/attachments/793772583946027050/801479779441967144/Z.png"
-                    )
-                    .setColor("BLUE")
-                    .setFooter(
-                        this.client.user.username,
-                        this.client.user.displayAvatarURL()
+                if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+                    return message.channel.send(
+                        "I don't have permissions to join and speak in that voice channel!"
                     );
-                message.channel.send(radio_play_embed);
-            });
+                }
+
+                voiceChannel.join().then((connection) => {
+                    connectionDispatcher = connection.play(
+                        "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3"
+                    );
+
+                    const radio_play_embed = new Discord.MessageEmbed()
+                        .setTitle("Started Playing the Radio!")
+                        .setDescription(
+                            "**:white_check_mark: Joined the Voice Channel and Playing the radio.** \n \n *Powered by FreeCodeCamp Radio*"
+                        )
+                        .setThumbnail(
+                            "https://media.discordapp.net/attachments/793772583946027050/801479779441967144/Z.png"
+                        )
+                        .setColor("BLUE")
+                        .setFooter(
+                            this.client.user.username,
+                            this.client.user.displayAvatarURL()
+                        );
+                    message.channel.send(radio_play_embed);
+                });
+            }
+
+            if (cmd == "stop") {
+                voiceChannel.leave();
+                message.channel.send(
+                    ":white_check_mark: Stopped the Radio and left the Voice channel!"
+                );
+            }
+        } catch (err) {
+            console.error(err);
         }
     }
 };
