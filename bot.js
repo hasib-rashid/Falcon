afbbab5f5e7210d129a417de0212c73d26358eed
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-03-27 23:16:55 +0600

    🍀Removing EmbedBuilder

--- bot.js
+++ bot.js
@@ -262,35 +262,6 @@ client.on("message", async (message) => {
         await message.channel.bulkDelete(2);
         return;
     }
-
-    function embedbuilder(
-        client,
-        message,
-        color,
-        title,
-        description,
-        thumbnail
-    ) {
-        try {
-            let embed = new Discord.MessageEmbed()
-                .setColor(color)
-                .setAuthor(
-                    message.author.tag,
-                    message.member.user.displayAvatarURL({ dynamic: true }),
-                    "https://harmonymusic.tk"
-                )
-                .setFooter(
-                    client.user.username,
-                    client.user.displayAvatarURL()
-                );
-            if (title) embed.setTitle(title);
-            if (description) embed.setDescription(description);
-            if (thumbnail) embed.setThumbnail(thumbnail);
-            return message.channel.send(embed);
-        } catch (error) {
-            console.error;
-        }
-    }
 });
 
 client.on("messageReactionAdd", async (reaction, user, msg) => {
