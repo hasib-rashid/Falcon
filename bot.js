de7bf0b3fb983b576dd26cd5b3d558309ffae191
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-03-28 23:25:48 +0600

    ❌Removing Unused Message Event

--- bot.js
+++ bot.js
@@ -150,27 +150,6 @@ client.on("ready", async () => {
     });
 });
 
-client.on("message", async (message) => {
-    const args = message.content.slice(1).trim().split(/ +/g);
-    const command = args.shift();
-
-    if (command === "!deleteRR") {
-        const emoji = args[0];
-        if (!emoji)
-            return message
-                .reply("You need use a valid emoji.")
-                .then((m) => m.delete({ timeout: 1000 }));
-
-        const msg = await message.channel.messages.fetch(args[1]);
-        if (!msg)
-            return message
-                .reply("Message not found! Wtf...")
-                .then((m) => m.delete({ timeout: 1000 }));
-
-        await reactionRoleManager.deleteReactionRole({ message: msg, emoji });
-    }
-});
-
 client.on("messageReactionAdd", async (reaction, user, msg) => {
     if (user.partial) await user.fetch();
     if (reaction.partial) await reaction.fetch();
