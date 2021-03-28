6a48a780e1089f2a07322a027a544e6f92c67696
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-03-28 15:36:02 +0600

    🎶Queue Command

--- commands/music/getGuildQueue.js
+++ commands/music/getGuildQueue.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const commando = require("discord.js-commando");
 const oneLine = require("common-tags").oneLine;
-const embedbuilder = require("../../util/embedBuilder.js");
+const embedbuilder = require("../../util/embedBuilder");
 
 module.exports = class ClassName extends commando.Command {
     constructor(client) {
@@ -24,20 +24,33 @@ module.exports = class ClassName extends commando.Command {
     async run(message) {
         try {
             let queue = this.client.distube.getQueue(message);
-            message.channel.send(
-                "Current queue:\n" +
-                    queue.songs
-                        .map(
-                            (song, id) =>
-                                `**${id + 1}**. [${song.name}](${
-                                    song.url
-                                }) - \`${song.formattedDuration}\``
-                        )
-                        .join("\n")
+
+            let embed = new Discord.MessageEmbed()
+                .setAuthor(message.guild.name, message.guild.iconURL())
+                .setTitle("Current Queue")
+                .setFooter(
+                    this.client.user.username,
+                    this.client.user.displayAvatarURL()
+                )
+                .setColor("#4287f5");
+
+            embed.setDescription(
+                queue.songs
+                    .map(
+                        (song, id) =>
+                            `**${id + 1}**. [${song.name}](${song.url}) - \`${
+                                song.formattedDuration
+                            }\``
+                    )
+                    .join("\n")
             );
+
+            message.channel.send(embed);
         } catch (err) {
-            console.log(err);
-            message.channel.send("**An Unexpected Error Occured**");
+            console.error(err);
+            message.channel.send(
+                "**An Unexpected Error Occured. There might be no songs playing.**"
+            );
         }
     }
 };
