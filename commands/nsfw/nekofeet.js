6d30ab4365139747d48016e131dbd1bb3bafbcdf
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-13 22:27:50 +0600

    🔞NekoFeet

---
+++ commands/nsfw/nekofeet.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const commando = require("discord.js-commando");
+const oneLine = require("common-tags").oneLine;
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+module.exports = class ClassName extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "nekofeet",
+            aliases: [],
+            group: "nsfw",
+            memberName: "nekofeet",
+            description: "nekofeet??",
+            details: oneLine`
+                nekofeet?????? idk
+            `,
+            examples: ["!nekofeet"],
+        });
+    }
+
+    /**
+     * @param {commando.CommandoMessage} message
+     */
+    async run(message) {
+        if (!message.channel.nsfw)
+            return message.channel.send(
+                "**:underage: NSFW Command. Please switch to NSFW channel in order to use this command.**"
+            );
+
+        const image = await nsfw.nekofeet();
+
+        message.say(image);
+    }
+};
