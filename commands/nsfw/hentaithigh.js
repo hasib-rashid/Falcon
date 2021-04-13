1d4fffe8fc0c678f424200febc317e29b1324c3f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-13 15:32:56 +0600

    🔞Hentai Thigh

---
+++ commands/nsfw/hentaithigh.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const commando = require("discord.js-commando");
+const oneLine = require("common-tags").oneLine;
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+module.exports = class ClassName extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "hentaithigh",
+            aliases: [],
+            group: "nsfw",
+            memberName: "hentaithigh",
+            description: "hentaithigh??",
+            details: oneLine`
+                hentaithigh?????? idk
+            `,
+            examples: ["!hentaithigh"],
+        });
+    }
+
+    /**
+     * @param {commando.CommandoMessage} message
+     */
+    async run(message) {
+        if (!message.channel.nsfw)
+            return message.channel.send(
+                "**:underage: NSFW Command. Please switch to NSFW channel in order to use this command.**"
+            );
+
+        const image = await nsfw.hentaithigh();
+
+        message.say(image);
+    }
+};
