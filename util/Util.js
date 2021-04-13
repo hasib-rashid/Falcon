831c31d2c11b5ba6764160fb5e6b04c17958a1af
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-13 23:05:22 +0600

    😶Util Fixes

--- util/Util.js
+++ util/Util.js
@@ -56,8 +56,8 @@ module.exports = class Util {
         const len = arr.length;
         if (len === 0) return "";
         if (len === 1) return arr[0];
-        return `${arr.slice(0, -1).join(", ")}${
-            len > 1 ? `${len > 2 ? "," : ""} ${conj} ` : ""
+        return `${arr.slice(0, -1).join("\n ")}${
+            len > 1 ? `${len > 2 ? "" : ""} ${conj} ` : ""
         }${arr.slice(-1)}`;
     }
 
