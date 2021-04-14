7b74bc59cd51ec980ef83fb14ed5de95a275c802
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-14 14:06:57 +0600

    🤣Meme Getting Data

---
+++ commands/general/meme.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const commando = require("discord.js-commando");
+const oneLine = require("common-tags").oneLine;
+const axios = require("axios").default;
+
+module.exports = class ClassName extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "meme",
+            aliases: [],
+            group: "general",
+            memberName: "meme",
+            description: "Watch a meme",
+            details: oneLine`
+                Watch a meme
+            `,
+            examples: ["!meme"],
+        });
+    }
+
+    /**
+     * @param {commando.CommandoMessage} message
+     */
+    async run(message) {
+        const url = "https://some-random-api.ml/meme";
+
+        axios.get(url).then(function (response) {
+            console.log(response.data);
+        });
+    }
+};
