11c10ad41367664aae86bd5c42a9783ceecf1bab
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-04-15 22:34:46 +0600

    📦SetWelcomeChannel done

--- commands/admin/setWelcomeChannel.js
+++ commands/admin/setWelcomeChannel.js
@@ -22,27 +22,34 @@ module.exports = class ClassName extends commando.Command {
      * @param {commando.CommandoMessage} message
      */
     async run(message) {
-        if (!message.member.hasPermission("MANAGE_CHANNELS"))
-            return message.channel.send(
-                "**You do not have the permission to use this command**"
-            );
-
-        const channel = message.mentions.channels.first() || message.channel;
-
-        setWelcomeChannelSchema.findOne(
-            { guild: message.guild.id },
-            async (err, data) => {
-                if (data) data.delete();
-
-                new setWelcomeChannelSchema({
-                    guild: message.guild.id,
-                    channel: channel,
-                }).save();
-
-                message.channel.send(
-                    `**The Welcome channel has been set to ${channel}!**`
+        try {
+            if (!message.member.hasPermission("MANAGE_CHANNELS"))
+                return message.channel.send(
+                    "**You do not have the permission to use this command**"
                 );
-            }
-        );
+            const channel = message.mentions.channels.first().id;
+
+            console.log(channel);
+
+            setWelcomeChannelSchema.findOne(
+                { guild: message.guild.id },
+                async (err, data) => {
+                    if (data) data.delete();
+
+                    new setWelcomeChannelSchema({
+                        guild: message.guild.id,
+                        channel: channel,
+                    }).save();
+
+                    message.channel.send(
+                        `**The Welcome channel has been set to <#${channel}>!**`
+                    );
+                }
+            );
+        } catch (err) {
+            message.channel.send(
+                "**An unexpected Error occured. Please make sure you mentioned the channel where to send the welcome messages.**"
+            );
+        }
     }
 };
