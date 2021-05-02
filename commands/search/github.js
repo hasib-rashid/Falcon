8f498c091dbb5edf2e20ca2c4f91e09b523d2827
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-03 03:41:18 +0600

    🧠 Logics

--- commands/search/github.js
+++ commands/search/github.js
@@ -26,7 +26,17 @@ module.exports = class GithubCommand extends commando.Command {
 
     async run(message) {
         try {
+            const args = message.content.split(" ").slice(1);
 
+            if (!args) return message.channel.send("**Please Specify the username and the repo or just the username of the github profile you want. Example: `github {username}` or `github {username} {repo-name}`**")
+
+            if (args[0]) {
+
+            }
+
+            if (!args[1]) {
+
+            }
         } catch (err) {
             if (err.status === 404)
                 return message.say("❌ Could not find any results.");
