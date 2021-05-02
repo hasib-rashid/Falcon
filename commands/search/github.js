cfb8a2cc7547c8ec8b6b2fe7f30ee3b2ff64f099
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-03 03:56:59 +0600

    🧠 Logics and fetching API

--- commands/search/github.js
+++ commands/search/github.js
@@ -30,12 +30,16 @@ module.exports = class GithubCommand extends commando.Command {
 
             if (!args) return message.channel.send("**Please Specify the username and the repo or just the username of the github profile you want. Example: `github {username}` or `github {username} {repo-name}`**")
 
-            if (args[0]) {
-
+            if (!args[1]) {
+                axios.get(`https://api.github.com/users/${args[0]}`).then(function (response) {
+                    console.log(response.data)
+                })
             }
 
-            if (!args[1]) {
-
+            if (args[1]) {
+                axios.get(`https://api.github.com/repos/${args[0]}/${args[1]}`).then(function (response) {
+                    console.log(response.data)
+                })
             }
         } catch (err) {
             if (err.status === 404)
