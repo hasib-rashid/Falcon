b8d3cf7084b553368e9305783fde074ec9f9fc35
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-03 02:39:27 +0600

    🙏Ban Designing Done

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { confirmation } = require("reconlx");
 const commando = require("discord.js-commando");
 const oneLine = require("common-tags").oneLine;
 
@@ -14,13 +15,6 @@ module.exports = class BanCommand extends commando.Command {
                 Bans a user you want to ban!
             `,
             examples: ["!ban <user>"],
-            args: [
-                {
-                    key: "reason",
-                    type: "string",
-                    prompt: "Please specify the reason",
-                },
-            ],
         });
     }
 
@@ -28,35 +22,36 @@ module.exports = class BanCommand extends commando.Command {
      * @param {commando.CommandoMessage} message
      */
 
-    async run(message, { reason }) {
+    async run(message) {
         try {
+            const args = message.content.split(" ").slice(2);
+
+            console.log(args.join(" "))
+
             if (!message.member.hasPermission("BAN_MEMBERS"))
                 return message.channel.send(
                     "**You need `BAN_MEMBERS` permission to use this command**"
                 );
-            const member = message.mentions.members.first();
 
-            if (!member)
-                return message.channel.send(":no_entry: No user mentioned.");
+            const targetUser = message.mentions.members.first() || (await message.guild.members.fetch('133990844666609664')).user
 
-            if (!member.kickable)
-                return message.channel.send(
-                    ":no_entry: I cannot ban this user."
-                );
-            if (member) {
-                if (reason) {
-                    member.ban().then((member) => {
-                        const banned_embed = new Discord.MessageEmbed()
-                            .setColor("RED")
-                            .setTitle("Banned Succesfully!")
-                            .setAuthor(`Banned by ${message.author.username}`)
-                            .setDescription(
-                                `${member.user.tag} was banned by ${message.author} for ${reason}.`
-                            );
-                        message.channel.send(banned_embed);
-                    });
+            const confirmEmbed = new Discord.MessageEmbed()
+                .setAuthor(message.author.username, message.author.displayAvatarURL())
+                .setTitle("Banning A User")
+                .setColor("#ff2424")
+                .setDescription(`**Are you sure you want to ban  ${targetUser}\n\nReason:  \`${args.join(" ")}\`\n\n This Action is irreversable.\n\n React with ✅ if you want to ban this user. And react with ❌ if you want to cancel this request.\n You have 30 seconds to apply the command.**`)
+                .setFooter(message.client.user.username, message.client.user.displayAvatarURL())
+
+            message.channel.send(confirmEmbed).then(async (msg) => {
+                const emoji = await confirmation(msg, message.author, ["✅", "❌"], 30000);
+
+                if (emoji === "✅") {
+                    message.channel.send("SimBan")
                 }
-            }
+                if (emoji === "❌") {
+                    message.channel.send("SimCancel")
+                }
+            })
         } catch (err) {
             console.error(err);
         }
