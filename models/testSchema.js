73d58ffed207240277e5d2db688bd899c56d2b28
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-06 22:20:30 +0600

    ❌ Delete Models

--- models/testSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const testSchema = new mongoose.Schema({
-    userID: String,
-    GuildID: String,
-    coins: String,
-});
-
-module.exports = mongoose.model("something", testSchema);
