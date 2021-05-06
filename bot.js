350da88db5d143f86216d0bfff4f2f4a3aa402ce
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-06 23:24:10 +0600

    ✅ DB Init

--- bot.js
+++ bot.js
@@ -7,6 +7,12 @@ const canvas = require("discord-canvas");
 const path = require("path");
 const { ReactionRoleManager } = require("discord.js-collector");
 const { formatNumber } = require("./util/Util");
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql",
+});
 
 const { GiveawaysManager } = require("discord-giveaways");
 
