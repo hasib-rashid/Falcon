a4f20df4fbe7d82c82e4fc99f1bfe2d53deb9b64
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-06 14:23:28 +0600

    ✅ Github User Fix

--- commands/search/github.js
+++ commands/search/github.js
@@ -33,6 +33,18 @@ module.exports = class GithubCommand extends commando.Command {
 
         if (!args[1]) {
             axios.get(`https://api.github.com/users/${args[0]}`).then(function (response) {
+                const public_repos = response.data.public_repos || "None"
+                const public_gists = response.data.public_gists || "None"
+                const followers = response.data.followers || "None"
+                const following = response.data.following || "None"
+                const created_at = response.data.created_at
+                const company = response.data.company || "No Comany"
+                const website = response.data.blog || "No Website"
+                const location = response.data.location || "No location Specified"
+                const email = response.data.email || "No Email"
+                const profile = response.data.html_url
+                const id = response.data.id
+
                 const userEmbed = new Discord.MessageEmbed()
                     .setAuthor("Github", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
                     .setDescription("**Description: **" + response.data.bio
@@ -41,17 +53,17 @@ module.exports = class GithubCommand extends commando.Command {
                     .setColor("#2577fa")
                     .setThumbnail(response.data.avatar_url)
                     .addFields(
-                        { name: "Public Repositories", value: response.data.public_repos, inline: true },
-                        { name: "Public Gists", value: response.data.public_gists, inline: true },
-                        { name: "Followers", value: response.data.followers, inline: true },
-                        { name: "Following", value: response.data.following, inline: true },
-                        { name: "Created At", value: moment.utc(response.data.created_at).format("MM/DD/YYYY h:mm A"), inline: true },
-                        { name: "Company", value: response.data.company, inline: true },
-                        { name: "Website", value: response.data.blog, inline: true },
-                        { name: "Location", value: response.data.location, inline: true },
-                        { name: "Email", value: response.data.email, inline: true },
-                        { name: "Profile", value: `[Link](${response.data.html_url})`, inline: true },
-                        { name: "ID", value: response.data.id, inline: true },
+                        { name: "Public Repositories", value: public_repos, inline: true },
+                        { name: "Public Gists", value: public_gists, inline: true },
+                        { name: "Followers", value: followers, inline: true },
+                        { name: "Following", value: following, inline: true },
+                        { name: "Created At", value: moment.utc(created_at).format("MM/DD/YYYY h:mm A"), inline: true },
+                        { name: "Company", value: company, inline: true },
+                        { name: "Website", value: website, inline: true },
+                        { name: "Location", value: location, inline: true },
+                        { name: "Email", value: email, inline: true },
+                        { name: "Profile", value: `[Link](${profile})`, inline: true },
+                        { name: "ID", value: id, inline: true },
                     )
 
                 if (response.data.name === null) {
