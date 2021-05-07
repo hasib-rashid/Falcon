0480c1da7e9c91b41ec8bf17d3fdfac0beef208e
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-07 15:34:20 +0600

    ✅ Database

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const canvas = require("discord-canvas");
 const path = require("path");
 const { ReactionRoleManager } = require("discord.js-collector");
 const { formatNumber } = require("./util/Util");
+const db = require("./database")
 
 const { GiveawaysManager } = require("discord-giveaways");
 
@@ -106,6 +107,12 @@ client.registry
 
 client.once("ready", () => {
     console.log(`[READY] Logged in as ${client.user.tag}!`);
+
+    db.authenticate().then(() => {
+        console.log("Connected to Database.")
+    }).catch(err => {
+        console.error(err)
+    })
 });
 
 // Triggered when the bot doesn't have permissions to manage this role.
--- database.js
+++ database.js
@@ -4,5 +4,5 @@ const { Sequelize } = require("sequelize")
 
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: "mysql",
-    host: process.env.DB_HOST
+    host: process.env.DB_HOSTNAME,
 })
