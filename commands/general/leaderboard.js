8542ddabc3d2c629dce7c2ed30fb1a5e04c3bf21
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-14 11:52:39 +0600

    ✅ Fetching Data

---
+++ commands/general/leaderboard.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const commando = require('discord.js-commando');
+const oneLine = require('common-tags').oneLine;
+const GuildUser = require("../../models/GuildUser")
+
+module.exports = class ClassName extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'leaderboard',
+            aliases: [],
+            group: 'general',
+            memberName: 'leaderboard',
+            description: 'Check the leaderboard here!',
+            details: oneLine`
+                Check the leaderboard here!
+            `,
+            examples: ['!leaderboard'],
+        })
+    }
+
+    /**
+     * @param {commando.CommandoMessage} message
+    */
+    async run(message) {
+        const leaderboard_users = await GuildUser.findAll()
+
+        console.log(leaderboard_users.dataValues)
+    }
+}
