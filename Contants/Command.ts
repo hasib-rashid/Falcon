1ce19bb5d28f1160f84cd0f70611edcb720792df
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-16 15:09:33 +0600

    👑 Constant Client

---
+++ Contants/Command.ts
@@ -0,0 +1,29 @@
+import MaverickClient from "../Classes/Client";
+import { Message } from "discord.js";
+
+interface RunFunction {
+    // eslint-disable-next-line no-unused-vars
+    (client: MaverickClient, message: Message, args: string[]): Promise<any>,
+}
+
+interface Credit {
+    name: string,
+    reason: string,
+    URL?: string,
+};
+
+export default interface Command {
+    name: string,
+    description: string,
+    aliases?: string[],
+    guildOnly?: boolean,
+    ownerOnly?: boolean,
+    disabled?: boolean,
+    nsfw?: boolean,
+    category?: string,
+    cooldown?: number,
+    credit?: Credit[],
+    args?: number,
+    run: RunFunction,
+    // eslint-disable-next-line
+}
