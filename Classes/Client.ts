7d6e0798cbc8a5b6d0b5b41335f3e0ec16367c7f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-16 15:13:16 +0600

    ✅ Contants Client

---
+++ Classes/Client.ts
@@ -0,0 +1,79 @@
+import { Client, Intents, Collection } from "discord.js";
+import { readdirSync } from "fs";
+import Config from "../Constants/Config";
+import Logger from "../Helpers/Logger";
+import Command from "../Constants/Command";
+import Event from "../Constants/Event";
+import NewMessageEmbed from "../Helpers/MessageEmbed";
+
+export default class MaverickClient extends Client {
+    public prefix: string;
+    public commands: Collection<string, Command> = new Collection();
+    public aliases: Collection<string, string> = new Collection();
+    public logger = Logger;
+    public ownerID: string;
+    public emotes;
+    public MessageEmbed = NewMessageEmbed;
+    // @ts-ignore
+    public countryCache: CountryResponse[];
+
+    constructor(config: Config) {
+        super({
+            ws: {
+                intents: Intents.ALL,
+                properties: { $browser: "Discord Android" },
+            },
+            disableMentions: "everyone",
+        });
+
+        this.prefix = config.prefix || "`";
+        this.ownerID = config.ownerID;
+        this.emotes = config.emotes;
+
+        this._loadCommands(config.commandDir);
+        this._loadEvents(config.eventDir);
+
+        this.login(config.token);
+    }
+
+    private _loadCommands(commandDir: string): void {
+        readdirSync(commandDir)
+            .forEach(async (dir: string) => {
+                const files = readdirSync(`${commandDir}/${dir}`);
+
+                for (const file of files) {
+                    const pseudoPull = await import(`${commandDir}/${dir}/${file}`);
+
+                    const pull: Command = pseudoPull.default;
+
+                    pull.category = dir;
+                    if (!pull.credit) pull.credit = [];
+
+                    pull.credit.push({
+                        name: "Defective Detective",
+                        reason: "Code",
+                        URL: "https://github.com/SpiderMath/Intrepid",
+                    });
+
+                    this.commands.set(pull.name.toLowerCase(), pull);
+
+                    if (pull.aliases) for (const alias of pull.aliases) this.aliases.set(alias.toLowerCase(), pull.name.toLowerCase());
+
+                    this.logger.success("client/commands", `Loaded command ${pull.name.toLowerCase()} successfully`);
+                }
+            });
+    }
+
+    private _loadEvents(eventDir: string) {
+        readdirSync(eventDir)
+            .forEach(async (file: string) => {
+                const pseudoPull = await import(`${eventDir}/${file}`);
+
+                const pull: Event = pseudoPull.default;
+
+                this.on(pull.name, pull.run.bind(null, this));
+
+                this.logger.success("client/events", `Listening for Event: ${pull.name}`);
+            });
+    }
+}
