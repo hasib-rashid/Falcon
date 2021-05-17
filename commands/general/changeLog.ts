26bad0024588c36fcd523e5987760fa3cb2cb983
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-17 14:03:17 +0600

    ✅ChangeLog

---
+++ commands/general/changeLog.ts
@@ -0,0 +1,51 @@
+import Command from '../../constants/command';
+import { MessageEmbed } from 'discord.js'
+import { stripIndents } from 'common-tags'
+
+const ChangeLogCommand: Command = {
+    name: 'changelog',
+    description: 'Watch the changelog of the bot here!',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        const embed = new MessageEmbed()
+            .setAuthor(
+                client.user?.username,
+                client.user?.displayAvatarURL()
+            )
+            .setTitle("ChangeLog V1.0.0. The Beginning!")
+            .setDescription(
+                stripIndents(
+                    `This is the Begining of CodeVert. SOOO Excited. An All in one Discord Bot!
+                    
+                    Added a Buncha Moderation Commands
+                    Fully end to end music experience
+                    7 new Games
+                    Translation Feature
+                    Searching anything you want
+                    Do Giveaways
+                    Transcript
+                    Ticket System
+                    Nuke Command
+                    Reaction Roles
+                    Welcome Message
+                    
+                    And Much More........`
+                )
+            )
+            .setColor("#0873ff")
+            .setTimestamp()
+            .setFooter("© Falcon");
+
+        message.channel.send(embed);
+    },
+}
+
+export default ChangeLogCommand;
