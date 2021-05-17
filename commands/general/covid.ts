445a15afb0ce03cda6de4443672d4f5ec48133d7
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-17 23:13:15 +0600

    👑 Covid cmd beautified

--- commands/general/covid.ts
+++ commands/general/covid.ts
@@ -17,65 +17,63 @@ const CovidCommand: Command = {
 
     async run(client, message, args) {
         try {
-            if (!args) {
+            if (args) {
                 axios.get(`https://corona.lmao.ninja/v2/countries/${args[0]}?yesterday=true&strict=true&query`).then(({ data }) => {
                     const embed = new MessageEmbed()
-                        .setAuthor(
-                            "Covid 19",
-                            "https://media.discordapp.net/attachments/793772583946027050/815533789761503232/image-removebg-preview_6.png?width=498&height=498"
-                        )
-                        .setTitle("Covid 19 Information")
+                        .setTitle(`**${data.country}**`)
                         .setDescription(
-                            "View all the latest Covid 19 news in CodeVert. The situation of Covid is on the way."
+                            "**View all the latest Covid 19 news in Falcon. The situation of Covid is on the way.**"
                         )
+                        .setThumbnail(data.countryInfo.flag)
+                        .setImage("https://images-ext-2.discordapp.net/external/Xo8QZezbd8LLT_NhijeH0eeFWYs-0JpqnJgcOb6KXZE/https/i.imgur.com/tGAnKcH.png?width=1025&height=267")
                         .addFields(
                             {
-                                name: "❯ Total Cases",
-                                value: formatNumber(data.cases),
+                                name: "**❯ Total Cases**",
+                                value: `**${formatNumber(data.cases)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Total Deaths",
-                                value: formatNumber(data.deaths),
+                                name: "**❯ Total Deaths**",
+                                value: `**${formatNumber(data.deaths)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Total Recovery",
-                                value: formatNumber(data.recovered),
+                                name: "**❯ Total Recovery**",
+                                value: `**${formatNumber(data.recovered)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Cases Today",
-                                value: formatNumber(data.todayCases),
+                                name: "**❯ Cases Today**",
+                                value: `**${formatNumber(data.todayCases)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Deaths Today",
-                                value: formatNumber(data.todayDeaths),
+                                name: "**❯ Deaths Today**",
+                                value: `**${formatNumber(data.todayDeaths)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Recovery Today",
-                                value: formatNumber(data.todayRecovered),
+                                name: "**❯ Recovery Today**",
+                                value: `**${formatNumber(data.todayRecovered)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Active Cases",
-                                value: formatNumber(data.active),
+                                name: "**❯ Active Cases**",
+                                value: `**${formatNumber(data.active)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Critical Cases",
-                                value: formatNumber(data.critical),
+                                name: "**❯ Critical Cases**",
+                                value: `**${formatNumber(data.critical)}**`,
                                 inline: true,
                             },
                             {
-                                name: "❯ Tests Today",
-                                value: formatNumber(data.tests),
+                                name: "**❯ Tests Today**",
+                                value: `**${formatNumber(data.tests)}**`,
                                 inline: true,
                             }
                         )
-                        .setColor("BLUE");
+                        .setColor("#e6295f");
 
                     message.channel.send(embed);
                 })
