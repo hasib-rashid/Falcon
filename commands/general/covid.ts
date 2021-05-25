e35fad704bd129ea620d5b72ab2aab94f50ca4de
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-25 23:58:28 +0600

    👑Everything is working

--- commands/general/covid.ts
+++ commands/general/covid.ts
@@ -1,7 +1,10 @@
 import Command from '../../constants/command';
 import { MessageEmbed } from 'discord.js'
-import { default as axios } from 'axios'
+import * as axiosConfig from 'axios'
 import { formatNumber } from '../../util/Util'
+import consola from 'consola'
+
+const axios = axiosConfig.default
 
 const CovidCommand: Command = {
     name: 'covid',
@@ -16,132 +19,72 @@ const CovidCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-        try {
-            if (args) {
-                axios.get(`https://corona.lmao.ninja/v2/countries/${args[0]}?yesterday=true&strict=true&query`).then(({ data }) => {
-                    const embed = new MessageEmbed()
-                        .setTitle(`**${data.country}**`)
-                        .setDescription(
-                            "**View all the latest Covid 19 news in Falcon. The situation of Covid is on the way.**"
-                        )
-                        .setThumbnail(data.countryInfo.flag)
-                        .setImage("https://images-ext-2.discordapp.net/external/Xo8QZezbd8LLT_NhijeH0eeFWYs-0JpqnJgcOb6KXZE/https/i.imgur.com/tGAnKcH.png?width=1025&height=267")
-                        .addFields(
-                            {
-                                name: "**❯ Total Cases**",
-                                value: `**${formatNumber(data.cases)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Total Deaths**",
-                                value: `**${formatNumber(data.deaths)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Total Recovery**",
-                                value: `**${formatNumber(data.recovered)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Cases Today**",
-                                value: `**${formatNumber(data.todayCases)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Deaths Today**",
-                                value: `**${formatNumber(data.todayDeaths)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Recovery Today**",
-                                value: `**${formatNumber(data.todayRecovered)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Active Cases**",
-                                value: `**${formatNumber(data.active)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Critical Cases**",
-                                value: `**${formatNumber(data.critical)}**`,
-                                inline: true,
-                            },
-                            {
-                                name: "**❯ Tests Today**",
-                                value: `**${formatNumber(data.tests)}**`,
-                                inline: true,
-                            }
-                        )
-                        .setColor("#e6295f");
+        if (args) {
+            axios.get(`https://corona.lmao.ninja/v2/countries/${args[0]}?yesterday=true&strict=true&query`).then((response) => {
+                const embed = new MessageEmbed()
+                    .setTitle(`**${response.data.country}**`)
+                    .setDescription(
+                        "**View all the latest Covid 19 news in Falcon. The situation of Covid is on the way.**"
+                    )
+                    .setThumbnail(response.data.countryInfo.flag)
+                    .setImage("https://images-ext-2.discordapp.net/external/Xo8QZezbd8LLT_NhijeH0eeFWYs-0JpqnJgcOb6KXZE/https/i.imgur.com/tGAnKcH.png?width=1025&height=267")
+                    .addFields(
+                        {
+                            name: "**❯ Total Cases**",
+                            value: `**${formatNumber(response.data.cases)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Total Deaths**",
+                            value: `**${formatNumber(response.data.deaths)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Total Recovery**",
+                            value: `**${formatNumber(response.data.recovered)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Cases Today**",
+                            value: `**${formatNumber(response.data.todayCases)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Deaths Today**",
+                            value: `**${formatNumber(response.data.todayDeaths)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Recovery Today**",
+                            value: `**${formatNumber(response.data.todayRecovered)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Active Cases**",
+                            value: `**${formatNumber(response.data.active)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Critical Cases**",
+                            value: `**${formatNumber(response.data.critical)}**`,
+                            inline: true,
+                        },
+                        {
+                            name: "**❯ Tests Today**",
+                            value: `**${formatNumber(response.data.tests)}**`,
+                            inline: true,
+                        }
+                    )
+                    .setColor("#e6295f");
 
-                    message.channel.send(embed);
-                })
-            } else {
-                axios.get("https://corona.lmao.ninja/v2/all?yesterday").then(({ data }) => {
-                    const embed = new MessageEmbed()
-                        .setAuthor(
-                            message.author.username,
-                            message.author.displayAvatarURL()
-                        )
-                        .setTitle("Covid 19 Information")
-                        .setDescription(
-                            "View all the latest Covid 19 news in CodeVert. The situation of Covid is on the way."
-                        )
-                        .addFields(
-                            {
-                                name: "❯ Total Cases",
-                                value: data.cases,
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Total Deaths",
-                                value: formatNumber(data.deaths),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Total Recovery",
-                                value: formatNumber(data.recovered),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Cases Today",
-                                value: formatNumber(data.todayCases),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Deaths Today",
-                                value: formatNumber(data.todayDeaths),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Recovery Today",
-                                value: formatNumber(data.todayRecovered),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Active Cases",
-                                value: formatNumber(data.active),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Critical Cases",
-                                value: formatNumber(data.critical),
-                                inline: true,
-                            },
-                            {
-                                name: "❯ Tests Today",
-                                value: formatNumber(data.tests),
-                                inline: true,
-                            }
-                        )
-                        .setColor("BLUE");
+                message.channel.send(embed);
+            }).catch(() => { })
+        }
 
-                    message.channel.send(embed);
-                })
-            }
-        } catch (err) {
-            console.error(err);
+        if (!args[0]) {
+            axios.get("https://corona.lmao.ninja/v2/all?yesterday").then((response) => {
+                console.log(response.data)
+            })
         }
     },
 }
