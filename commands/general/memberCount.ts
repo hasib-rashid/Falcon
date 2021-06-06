fb76d702cbb96a1eeb0559c8e90b27dc270e02d0
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-06 17:42:08 +0600

    👀Member Count

---
+++ commands/general/memberCount.ts
@@ -0,0 +1,26 @@
+import Command from '../../constants/command';
+import { MessageEmbed } from 'discord.js'
+
+const MemberCountCommand: Command = {
+    name: 'memberCount',
+    description: 'Count the number of members here',
+    aliases: [
+        'member-count', 'membercount'
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        const embed = new MessageEmbed()
+            .setAuthor(message.guild?.name, message.guild?.iconURL() || "https://media.pocketgamer.biz/2021/5/110514/discord-new-logo-2021-r225x.jpg")
+            .setTitle("Number of Users: " + message.guild?.memberCount)
+            .setColor("BLUE");
+
+        message.channel.send(embed);
+    },
+}
+
+export default MemberCountCommand;
