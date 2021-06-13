f9ff61be19dccf315192b1c6a1947daad0657412
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-13 22:55:34 +0600

    📦Dont need them

--- database/config/sequelizeConfig.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const config = {
-    "database": process.env.DB_NAME,
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "hostname": process.env.DB_HOSTNAME
-}
