ffd0fc12ebc6f527bba3d4c93f3673fc4725acaf
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-13 16:35:53 +0600

    📦GuildUser Model done

--- models/GuildUser.ts
+++ models/GuildUser.ts
@@ -1,18 +1,43 @@
-import * as Sequelize from 'sequelize';
-import { SequelizeAttributes } from '../typings/SequelizeAttribues/index';
+import dotenv from 'dotenv'
+dotenv.config()
 
-export interface UserAttributes {
-    id?: number;
-    name: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-};
+import { Sequelize, Model, DataTypes } from "sequelize";
 
+const sequelize = new Sequelize(`mysql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:3306/${process.env.DB_NAME}`);
 
-export const UserFactory = (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes): Sequelize.Model<UserInstance, UserAttributes> => {
-    const attributes: SequelizeAttributes<UserAttributes> = {
-        name: {
-            type: DataTypes.
-      }
-    }
+class GuildUser extends Model {
+    public userID!: string;
+    public guildID!: string;
+    public rank?: string | null;
+    public rankCache?: number | null;
+    public level?: number | null;
 }
+
+GuildUser.init(
+    {
+        userID: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        guildID: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rank: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        rankCache: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        level: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        }
+    },
+    {
+        tableName: "guilduser",
+        sequelize,
+    }
+);
