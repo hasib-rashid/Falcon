48911c936d01a6e87a918e7f8b21514bfc694d11
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-15 23:08:37 +0600

    ✅Backup plans

--- commands/games/tttUtil.ts
+++
@@ -1,7 +0,0 @@
-export function isSubset(big: any, small: any) {
-    return small.every((v: any) => big.includes(v));
-}
-
-export function In(parent: any, match: any) {
-    return match.includes(parent);
-}
