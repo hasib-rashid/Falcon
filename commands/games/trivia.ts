76fe1c96c0fe64e2f266715098b1d5ef1c0ac6e5
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-16 15:41:30 +0600

    📦Backup plans

--- commands/games/trivia.ts
+++ commands/games/trivia.ts
@@ -1,6 +1,8 @@
 import Command from '../../constants/command';
-import { createCanvas } from 'canvas'
+import { stripIndents } from 'common-tags'
+import { shuffle } from '../../util/Util'
 import { default as axios } from 'axios'
+const choices = ['A', 'B', 'C', 'D'];
 
 const TriviaCommand: Command = {
     name: 'trivia',
@@ -15,22 +17,7 @@ const TriviaCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-        await axios.get("https://opentdb.com/api.php", {
-            params: {
-                amount: 1,
-                type: args[0],
-                encode: 'url3986',
-                difficulty: args[1]
-            }
-        }).then((res) => {
-            if (!res.data) return message.channel.send('**A question could not be fetched at the moment. Try again later!**');
 
-            const answers = res.data[0].incorrect_answers.map((answer: any) => decodeURIComponent(answer.toLowerCase()));
-            const correct = decodeURIComponent(res.data[0].correct_answer.toLowerCase());
-            answers.push(correct);
-
-            console.log(res.data)
-        })
     },
 }
 
