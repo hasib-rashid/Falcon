86034d14908b31d31e54d091155942c48863dbe0
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-20 23:49:13 +0600

    🔎Instagram done

---
+++ commands/search/instagram.ts
@@ -0,0 +1,57 @@
+import Command from '../../constants/command';
+import { default as axios } from 'axios'
+import { MessageEmbed } from 'discord.js'
+
+const InstagramCommand: Command = {
+    name: 'instagram',
+    description: 'Look at the ig of someone in Instagram',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        if (!args[0]) {
+            return message.channel.send(`Please Enter a Channel Name`)
+        }
+        let url, response, account, details;
+        try {
+            url = `https://instagram.com/${args[0]}/?__a=1`;
+            response = await axios.get(url)
+            account = response.data
+            details = account.graphql.user
+        } catch (error) {
+            return message.channel.send(`Not A Account`)
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`${details.is_verified ? `${details.username} <a:verified:727820439497211994>` : ` ${details.username}`} ${details.is_private ? '🔒' : ''} `)
+            .setDescription(details.biography)
+            .setThumbnail(details.profile_pic_url)
+            .addFields(
+                {
+                    name: "Total Posts:",
+                    value: details.edge_owner_to_timeline_media.count.toLocaleString(),
+                    inline: true
+                },
+                {
+                    name: "Followers:",
+                    value: details.edge_followed_by.count.toLocaleString(),
+                    inline: true
+                },
+                {
+                    name: "Following:",
+                    value: details.edge_follow.count.toLocaleString(),
+                    inline: true
+                }
+            )
+
+        message.channel.send(embed)
+    },
+}
+
+export default InstagramCommand;
