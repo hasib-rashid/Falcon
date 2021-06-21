5d38fc09831c04f744594dababc31ecfd70cbc8a
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-21 23:09:44 +0600

    🔎User Const

--- commands/search/github.ts
+++ commands/search/github.ts
@@ -17,7 +17,17 @@ const GithubCommand: Command = {
     async run(client, message, args) {
         if (!args[1]) {
             axios.get(`https://api.github.com/users/${args[0]}`).then(function (response) {
-
+                const public_repos = response.data.public_repos || "None"
+                const public_gists = response.data.public_gists || "None"
+                const followers = response.data.followers || "None"
+                const following = response.data.following || "None"
+                const created_at = response.data.created_at
+                const company = response.data.company || "No Comany"
+                const website = response.data.blog || "No Website"
+                const location = response.data.location || "No location Specified"
+                const email = response.data.email || "No Email"
+                const profile = response.data.html_url
+                const id = response.data.id
             })
         }
     },
