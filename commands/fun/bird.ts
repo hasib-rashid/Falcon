4dc94925310d3d0bc0e1d5e303ddbd4f472d7794
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-23 15:33:32 +0600

    🦅Bird Start

---
+++ commands/fun/bird.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const BirdCommand: Command = {
+    name: 'bird',
+    description: 'Watch some birds!',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default BirdCommand;
