c4802eb7c09e7150e8ba54c197a7a6b81bad0000
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-06-23 15:29:59 +0600

    🖋Done IG

--- commands/fun/hug.ts
+++ commands/fun/hug.ts
@@ -16,8 +16,14 @@ const HugCommand: Command = {
 
     async run(client, message, args) {
         axios.get("https://some-random-api.ml/animu/hug").then((res) => {
+            const mentioned = message.mentions.users.first() || message.guild?.members.cache.get(args[0])
+
             const embed = new MessageEmbed()
                 .setAuthor(message.author.username, message.author.displayAvatarURL())
+                .setDescription(`<@${message.author.id}> hugged <@${mentioned}>`)
+                .setImage(res.data.link)
+
+            message.channel.send(embed)
         })
     },
 }
