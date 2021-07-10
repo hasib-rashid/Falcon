0984591286a06f58d4f28a51f7c179cf2184ea3a
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-10 23:42:21 +0600

    🔭Fix

--- commands/search/giphy.ts
+++ commands/search/giphy.ts
@@ -19,7 +19,6 @@ const GiphyCommand: Command = {
     async run(client, message, args) {
         //@ts-ignore
         axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: args.join(""), api_key: process.env.GIPHY_API_KEY, rating: message.channel.nsfw ? "r" : "pg" } }).then((res) => {
-            console.log(res.data)
             if (!res.data.data.length)
                 return message.channel.send("Could not find any results.");
 
