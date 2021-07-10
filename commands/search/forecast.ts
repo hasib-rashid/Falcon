37c0831ea72c2c8dd2837c716bff183b096e57ae
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-10 15:34:21 +0600

    ☀Forecast is done

--- commands/search/forecast.ts
+++ commands/search/forecast.ts
@@ -1,7 +1,10 @@
 import Command from '../../constants/command';
+import { MessageEmbed } from 'discord.js'
+// @ts-ignore
+import weather from 'weather-js'
 
 const WeatherCommand: Command = {
-    name: 'weather',
+    name: 'forecast',
     description: 'Watch the weather anywhere!',
     aliases: [
         ''
@@ -13,7 +16,23 @@ const WeatherCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        weather.find({ search: args.join(" "), degreeType: 'C' }, function (err: Error, result: any) {
+            if (err) throw err
 
+            const embed = new MessageEmbed()
+                .setAuthor(
+                    `${result[0].location.name}`,
+                    "https://media.discordapp.net/attachments/793772583946027050/823774180305534986/image-removebg-preview_8.png"
+                )
+                .setThumbnail("https://media.discordapp.net/attachments/793772583946027050/823774180305534986/image-removebg-preview_8.png")
+                .setTimestamp()
+                .setColor("#037ffc");
+            for (var i = 0; i < result[0].forecast.length; ++i) {
+                const results = result[0].forecast[i]
+                embed.addField(results.day, `Highest: ${results.high}\nLowest: ${results.low}\nCondition: ${results.skytextday}`)
+            }
+            message.channel.send(embed);
+        });
     },
 }
 
