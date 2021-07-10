a8efa68b787a7ea884032262af59a77ec66d6d1f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-10 15:36:51 +0600

    ☀Weather create

---
+++ commands/search/weather.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const WeatherCommand: Command = {
+    name: 'weather',
+    description: 'Watch the weather anywhere',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default WeatherCommand;
