d22c6225394fdf2d6761b7eda1637bfbbaf9c6df
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-12 15:54:20 +0600

    🔭Init djs

---
+++ commands/search/discordjs.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const DiscordJSCommand: Command = {
+    name: 'djs',
+    description: 'Search the Djs docs here',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default DiscordJSCommand;
