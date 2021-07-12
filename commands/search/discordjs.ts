3f6ddc2fbcdf115dafebd8832507382480f0e0b8
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-12 16:07:47 +0600

    🔭Working yay

--- commands/search/discordjs.ts
+++ commands/search/discordjs.ts
@@ -1,5 +1,6 @@
 import Command from '../../constants/command';
 import { default as axios } from 'axios';
+import { MessageEmbed } from 'discord.js';
 
 const DiscordJSCommand: Command = {
     name: 'djs',
@@ -14,7 +15,18 @@ const DiscordJSCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        let version = args[1]
 
+        if (!version) {
+            version = "stable"
+        }
+        axios.get(`https://djsdocs.sorta.moe/v2/embed?src=${encodeURIComponent(version)}&q=${encodeURIComponent(args[0])}`).then((res) => {
+            console.log(res.data)
+
+            const embed = new MessageEmbed(res.data)
+
+            message.channel.send(embed)
+        })
     },
 }
 
