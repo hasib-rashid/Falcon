9ce927623813e7576f4126061048b4cf0a5382be
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-12 16:08:04 +0600

    🔭Done

--- commands/search/discordjs.ts
+++ commands/search/discordjs.ts
@@ -21,10 +21,7 @@ const DiscordJSCommand: Command = {
             version = "stable"
         }
         axios.get(`https://djsdocs.sorta.moe/v2/embed?src=${encodeURIComponent(version)}&q=${encodeURIComponent(args[0])}`).then((res) => {
-            console.log(res.data)
-
             const embed = new MessageEmbed(res.data)
-
             message.channel.send(embed)
         })
     },
