da446874af5dcb3bf2e477f8099fa574caa608f8
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-14 23:02:24 +0600

    🎶Fix Resume

--- commands/music/resume.ts
+++ commands/music/resume.ts
@@ -15,7 +15,7 @@ const ResumeCommand: Command = {
     async run(client, message, args) {
         try {
             client.distube.resume(message)
-            message.channel.send("**Successfully Paused the current song!**");
+            message.channel.send("**Successfully Resumed the current song!**");
         } catch (err) {
             message.channel.send(
                 "**Either you are not in a voice channel or a song is not playing or the song is not paused**"
