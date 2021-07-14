d2a6db377a8ddb3a97e6213d73ca7a2235000e1d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-14 23:11:14 +0600

    🎶PlaySkip Done

---
+++ commands/music/playSkip.ts
@@ -0,0 +1,24 @@
+import Command from '../../constants/command';
+
+const PlaySkipCommand: Command = {
+    name: 'playskip',
+    description: 'Play and skip a song',
+    aliases: [
+        'play-skip', 'ps'
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        try {
+            client.distube.playSkip(message);
+        } catch (err) {
+            message.channel.send("**You are not in a voice channel!**");
+        }
+    },
+}
+
+export default PlaySkipCommand;
