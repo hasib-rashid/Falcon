b356195319c6243224d8751cb7282697ab7191b0
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-14 23:02:42 +0600

    🎶Fix Skip

--- commands/music/skip.ts
+++ commands/music/skip.ts
@@ -15,7 +15,7 @@ const SkipCommand: Command = {
     async run(client, message, args) {
         try {
             client.distube.skip(message)
-            message.channel.send("**Successfully Paused the current song!**");
+            message.channel.send("**Successfully Skipped the current song!**");
         } catch (err) {
             message.channel.send(
                 "**Either you are not in a voice channel or a song is not playing.**"
