dbd6faf7e04ea49525c6904624159ab766457a69
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-16 23:07:04 +0600

    ✅Clean Test

--- commands/general/test.ts
+++ commands/general/test.ts
@@ -1,6 +1,4 @@
 import Command from '../../constants/command';
-import GuildUser from '../../models/GuildUser'
-import { AdminCommands, GeneralCommands } from '../../classes/client'
 
 const TestCommand: Command = {
     name: 'test',
@@ -15,9 +13,7 @@ const TestCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-        for (var i = 0; i < GeneralCommands.length; ++i) {
-            console.log(GeneralCommands[0][i])
-        }
+
     },
 }
 
