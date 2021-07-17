753d604951232736c4d99e35250bfcc993084174
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-17 13:43:41 +0600

    📚Calculator

--- commands/search/country.ts
+++ commands/search/country.ts
@@ -1,5 +1,7 @@
 import Command from '../../constants/command';
 import { default as axios } from 'axios'
+import { formatNumber } from '../../util/Util';
+import { MessageEmbed } from 'discord.js';
 
 const CountryCommand: Command = {
     name: 'country',
@@ -14,8 +16,23 @@ const CountryCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-        axios.get(`https://restcountries.eu/rest/v2/name/${args[0]}`).then((res) => {
-            console.log(res.data)
+        axios.get(`https://restcountries.eu/rest/v2/name/${args[0]}`).then(({ data }) => {
+            const embed = new MessageEmbed()
+                .setColor(0x00ae86)
+                .setTitle(data.name)
+                .setThumbnail(data[0].flag)
+                .addField("❯ Population", formatNumber(data[0].population), true)
+                .addField("❯ Capital", data[0].capital || "None", true)
+                .addField("❯ Currency", data[0].currencies[0].symbol, true)
+                .addField("❯ Location", data[0].subregion || data.region, true)
+                .addField("❯ Demonym", data[0].demonym || "None", true)
+                .addField("❯ Native Name", data[0].nativeName, true)
+                .addField("❯ Area", `${formatNumber(data[0].area)}km`, true)
+                .addField(
+                    "❯ Languages",
+                    data[0].languages.map((lang: any) => lang.name).join("/")
+                );
+            return message.channel.send(embed);
         })
     },
 }
