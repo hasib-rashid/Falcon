9d1a54499c9460cb4e967bb119dce3159580b3ef
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-17 23:52:46 +0600

    ✅Info Is Finally Done Here

--- commands/misc/info.ts
+++ commands/misc/info.ts
@@ -24,6 +24,17 @@ const InfoCommand: Command = {
 
     async run(client, message, args) {
         try {
+            // @ts-ignore
+            let totalSeconds = (client.uptime / 1000);
+            let days = Math.floor(totalSeconds / 86400);
+            totalSeconds %= 86400;
+            let hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+            let minutes = Math.floor(totalSeconds / 60);
+            let seconds = Math.floor(totalSeconds % 60);
+
+            const uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
             const embed = new MessageEmbed()
                 .setColor("GREEN")
                 .setAuthor(
@@ -31,11 +42,11 @@ const InfoCommand: Command = {
                     message.author.displayAvatarURL()
                 )
                 .setDescription(
-                    "Thanks for using CodeVert! This bot is to make your life simple and fun! Enjoy the many features CodeVert has to offer like Moderating, Music, Games, Events, and more!"
+                    "Thanks for using Falcon! This bot is to make your life simple and fun! Enjoy the many features CodeVert has to offer like Moderating, Music, Games, Events, and more!"
                 )
                 .setColor("GREEN")
-                .setFooter(copyright.join("\n"))
-                .addField("❯ Owner", "TheMegaWarrior#3647", true)
+                .setFooter("©2021 Conqueror.js#3091 | Hasib Al Rashid")
+                .addField("❯ Owner", "Conqueror.js#3091", true)
                 .addField(
                     "❯ Servers",
                     formatNumber(`${client.guilds.cache.size}`),
@@ -58,14 +69,13 @@ const InfoCommand: Command = {
                 )
                 .addField(
                     "❯ Invite",
-                    "[Invite CodeVert](https://discord.com/api/oauth2/authorize?client_id=799543154692718602&permissions=8&scope=bot)",
+                    "[Invite Falcon](https://discord.com/api/oauth2/authorize?client_id=799543154692718602&permissions=8&scope=bot)",
                     true
                 )
                 .addField("❯ Source Code", "N/A", true)
                 .addField(
                     "❯ Uptime",
-                    // @ts-ignore
-                    moment.duration(client.uptime).format("d:hh:mm:ss"),
+                    uptime,
                     true
                 )
                 .addField("❯ Version", `v${version}`, true)
