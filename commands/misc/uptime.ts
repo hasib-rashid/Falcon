bda13f4de078aa48221356201c243b0f91d828ac
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-18 16:08:41 +0600

    ✅Uptime Done

--- commands/misc/uptime.ts
+++ commands/misc/uptime.ts
@@ -13,7 +13,16 @@ const UptimeCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-
+        // @ts-ignore
+        let totalSeconds = client.uptime / 1000;
+        let days = Math.floor(totalSeconds / 86400);
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = totalSeconds / 60;
+        message.channel.send(
+            `:low_brightness: **Uptime:** ${days} days, ${hours} hours and ${minutes} minutes!`
+        );
     },
 }
 
