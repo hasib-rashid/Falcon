b70fc348d550d3560a560b551155a652596ab671
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-19 23:27:11 +0600

    ✅Alot of improvements

--- commands/admin/ban.ts
+++ commands/admin/ban.ts
@@ -20,8 +20,10 @@ const BanCommand: Command = {
                 "**You need `BAN_MEMBERS` permission to use this command**"
             );
 
+        if (message.author.id === "839367177899737108") return message.channel.send("Imagine Dragons Deez Nutz right in your face.")
 
-        const banReason = args.slice(1).join(' ');
+
+        const banReason = args.slice(1).join(' ') || "No Reason";
 
         const targetUser = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
 
@@ -29,7 +31,7 @@ const BanCommand: Command = {
             .setAuthor(message.author.username, message.author.displayAvatarURL())
             .setTitle("Banning A User")
             .setColor("#ed3737")
-            .setDescription(`**Are you sure you want to ban  ${targetUser} for \`${banReason || "No Reason"}\`**`)
+            .setDescription(`**Are you sure you want to ban  ${targetUser} for \`${banReason}\`**`)
             .setFooter(message.client.user?.username, message.client.user?.displayAvatarURL())
 
         const confirmButton = new MessageButton()
@@ -53,6 +55,15 @@ const BanCommand: Command = {
                 if (!button.message.author) return;
 
                 button.message.channel.send("**Smulate Ban**")
+
+                const banEmbed = new MessageEmbed()
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setTitle(`Banned from ${message.guild?.name}`)
+                    .setDescription(`**${message.author} Has Banned you from ${message.guild?.name} for \`${banReason}\`. Please contact him if you want to get unbanned.**`)
+                    .setColor("#ed3737")
+                    .setFooter(client.user?.username, client.user?.displayAvatarURL())
+
+                targetUser?.send(banEmbed).catch((err) => { message.channel.send("**Messages wasnt sent to this user because this user has his DM's disabled.**") })
             }
 
             if (button.id === "ban-no") {
