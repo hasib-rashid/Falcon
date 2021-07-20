139de1307aab8bf9a147af88caa5b05a8c4ac496
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-20 23:19:30 +0600

    👑Lock Init

---
+++ commands/admin/lock.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const LockCommand: Command = {
+    name: 'lock',
+    description: 'Lock down a channel',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default LockCommand;
