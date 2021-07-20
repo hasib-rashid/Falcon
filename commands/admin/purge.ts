5a39a1edffa29b5dc1bd17248a2f9eb3f4f2ade3
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-20 16:00:50 +0600

    🔧Purge Command Done

--- commands/admin/purge.ts
+++ commands/admin/purge.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from 'discord.js';
 import Command from '../../constants/command';
 
 const PurgeCommand: Command = {
@@ -13,7 +14,51 @@ const PurgeCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        if (!message.member?.hasPermission("MANAGE_MESSAGES"))
+            return message.channel.send(
+                "**You need `MANAGE_MESSAGES` permission to use this command**"
+            );
 
+        if (message.member?.hasPermission("MANAGE_MESSAGES")) {
+            const args = message.content.split(" ").slice(1);
+            const amount = args.join(" ");
+
+            if (!amount)
+                return message.channel.send(
+                    "You haven't given an amount of messages which should be deleted!"
+                );
+
+            // @ts-ignore
+            if (isNaN(amount))
+                return message.channel.send(
+                    "The amount parameter isn`t a number!"
+                );
+
+            if (+amount > 100)
+                return message.channel.send(
+                    "You can`t delete more than 100 messages at once!"
+                );
+            if (+amount < 1)
+                return message.channel.send(
+                    "You have to delete at least 1 message!"
+                );
+
+            // @ts-ignore
+            message.channel.bulkDelete(amount);
+
+            const purgeEmbed = new MessageEmbed()
+                .setTitle("Operation Succesful")
+                .setAuthor(`By ${message.author.tag}`)
+                .setDescription(
+                    `${amount} Messages has been deleted! by ${message.author.tag}`
+                )
+                .setColor("GREEN")
+                .setThumbnail(message.author.displayAvatarURL());
+
+            message.channel.send(purgeEmbed).then((msg) => {
+                setTimeout(() => msg.delete(), 5000);
+            });
+        }
     },
 }
 
