84e954e007f90839a3087dffccbe6930f783044e
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-20 22:56:04 +0600

    👑RemoveRR Done I think

--- commands/admin/removeRR.ts
+++ commands/admin/removeRR.ts
@@ -13,7 +13,37 @@ const RemoveRRCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        try {
+            if (!message.member?.hasPermission("MANAGE_ROLES"))
+                return message.channel.send(
+                    "**You need `MANAGE_ROLES` permission to use this command**"
+                );
 
+            const args = message.content.split(" ").slice(1);
+
+            const emoji = args[0];
+            if (!emoji)
+                return message
+                    .reply("**You need use a valid emoji.**")
+                    .then((m) => m.delete({ timeout: 1000 }));
+
+            const msg = await message.channel.messages.fetch(args[1]);
+            if (!msg)
+                return message
+                    .reply("**Message not found! Wtf...**")
+                    .then((m) => m.delete({ timeout: 1000 }));
+
+            await client.reactionRole.deleteReactionRole({
+                message: msg,
+                emoji,
+            });
+
+            message.reply("Done").then((m) => m.delete({ timeout: 500 }));
+        } catch (err) {
+            message.channel.send(
+                "**An unexpected Error Occured. Use the correct format: `deleteRR <emoji> <messageID>`**"
+            );
+        }
     },
 }
 
