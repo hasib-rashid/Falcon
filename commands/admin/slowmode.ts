6c3a26b84c1d7b9912836af75d848698b41f1c31
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-20 23:12:10 +0600

    👑Slowmode is Now Done

--- commands/admin/slowmode.ts
+++ commands/admin/slowmode.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from 'discord.js';
 import Command from '../../constants/command';
 
 const SlowmodeCommand: Command = {
@@ -13,7 +14,64 @@ const SlowmodeCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        try {
+            let time = args[0]
+            const reason = args.slice(1).join(' ') || "No Reason";
 
+            if (!message.member?.hasPermission("MANAGE_CHANNELS"))
+                return message.channel.send(
+                    "**You need `MANAGE_CHANNELS` permission to use this command**"
+                );
+
+            const { channel } = message;
+
+            let duration = time;
+            if (duration === "off") {
+                // @ts-ignore
+                duration = 0;
+            }
+
+            if (isNaN(+duration)) {
+                let embed = new MessageEmbed()
+                    .setAuthor(
+                        message.author.tag,
+                        message.author.displayAvatarURL()
+                    )
+                    .setColor("RANDOM")
+                    .setDescription(
+                        'Please provide either a number of seconds or the word "off"'
+                    )
+                    .setThumbnail(
+                        "https://i.pinimg.com/originals/3f/82/40/3f8240fa1d16d0de6d4e7510b43b37ba.gif"
+                    )
+                    .setFooter("CodeVert 2021");
+
+                message.channel.send(embed);
+                return;
+            }
+
+            // @ts-ignore
+            channel.setRateLimitPerUser(duration, reason);
+
+            const embed = new MessageEmbed()
+                .setAuthor(
+                    message.author.tag,
+                    message.author.displayAvatarURL()
+                )
+                .setColor("RANDOM")
+                .setDescription(
+                    // @ts-ignore
+                    `The slowmode for this channel "${message.channel.name}" was successfully set to ${duration} for ${reason}`
+                )
+                .setThumbnail(
+                    "https://i.pinimg.com/originals/3f/82/40/3f8240fa1d16d0de6d4e7510b43b37ba.gif"
+                )
+                .setFooter("CodeVert 2021");
+
+            message.reply(embed);
+        } catch (err) {
+            console.error(err);
+        }
     },
 }
 
