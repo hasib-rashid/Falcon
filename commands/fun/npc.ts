0161d7491e54db01110630c5ed0701358953064c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-20 15:20:25 +0600

    ✅Fix

--- commands/fun/npc.ts
+++ commands/fun/npc.ts
@@ -3,7 +3,7 @@ import { default as axios } from 'axios'
 
 const NPCCommand: Command = {
     name: 'npc',
-    description: 'Get some Stonks',
+    description: 'Try to make a meme with NPC',
     aliases: [
         ''
     ],
@@ -15,6 +15,9 @@ const NPCCommand: Command = {
 
     async run(client, message, args) {
         const user = message.mentions.users.first()?.displayAvatarURL() || message.guild?.members.cache.get(args[0])?.user.displayAvatarURL() || message.author.displayAvatarURL()
+
+        if (args[0]) return message.channel.send("**Please insert two Text**")
+        if (args[1]) return message.channel.send("**Please insert two Text**")
 
         message.channel.send(`https://vacefron.nl/api/npc?text1=${args[0]}&text2=${args[1]}`)
     },
