cbf90a18c35ac954b6fb77a9404b6805cea28d18
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-21 22:56:43 +0600

    👑Done Mute

--- commands/admin/mute.ts
+++ commands/admin/mute.ts
@@ -14,14 +14,14 @@ const MuteCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        const muteReason = args.slice(1).join(' ') || "No Reason";
+
         if (!message.member?.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have permissions to use this command')
         const Member = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
         if (!Member) return message.channel.send('Member is not found.')
         const role = message.guild?.roles.cache.find(role => role.name.toLowerCase() === 'muted')
         if (!role) {
             try {
-                message.channel.send('Muted role is not found, attempting to create muted role.')
-
                 let muterole = await message.guild?.roles.create({
                     data: {
                         name: 'muted',
@@ -34,7 +34,6 @@ const MuteCommand: Command = {
                         ADD_REACTIONS: false
                     })
                 });
-                message.channel.send('Muted role has sucessfully been created.')
             } catch (error) {
                 console.log(error)
             }
@@ -44,7 +43,16 @@ const MuteCommand: Command = {
         if (Member.roles.cache.has(role2?.id)) return message.channel.send(`${Member.displayName} has already been muted.`)
         // @ts-ignore
         await Member.roles.add(role2)
-        message.channel.send(`${Member} is now muted.`)
+        message.channel.send(`**${Member} was successfully muted.**`)
+
+        const muteEmbed = new MessageEmbed()
+            .setAuthor(message.author.username, message.author.displayAvatarURL())
+            .setTitle(`Banned from ${message.guild?.name}`)
+            .setDescription(`**${message.author} Has Banned you from ${message.guild?.name} for \`${muteReason}\`. Please contact him if you want to get unbanned.**`)
+            .setColor("#ed3737")
+            .setFooter(client.user?.username, client.user?.displayAvatarURL())
+
+        Member?.send(muteEmbed).catch((err) => { message.channel.send("**Message wasn't sent to this user because this user has his DM's disabled.**") })
     },
 }
 
