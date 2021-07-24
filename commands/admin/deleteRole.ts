8d7fe6e0ffda9273ef2804511e9e9654863037a4
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-24 23:37:10 +0600

    👑Fix

--- commands/admin/deleteRole.ts
+++ commands/admin/deleteRole.ts
@@ -13,6 +13,11 @@ const DeleteRoleCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        if (!message.member?.hasPermission("BAN_MEMBERS"))
+            return message.channel.send(
+                "**You need `BAN_MEMBERS` permission to use this command**"
+            );
+
         const role = message.mentions.roles.first() || message.guild?.roles.cache.get(args[0])
 
         if (!role) return message.channel.send("**Please specify a role to delete**")
