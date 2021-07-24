179ecbe601146cfa2ba50215302fe5a078403a22
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-24 23:39:16 +0600

    👑Fix

--- commands/admin/unlock.ts
+++ commands/admin/unlock.ts
@@ -13,6 +13,11 @@ const UnlockCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        if (!message.member?.hasPermission("MANAGE_CHANNELS"))
+            return message.channel.send(
+                "**You need `MANAGE_CHANNELS` permission to use this command**"
+            );
+
         // @ts-ignore
         message.channel.updateOverwrite(message.guild?.roles.everyone, { SEND_MESSAGES: true }).then(() => {
             message.channel.send('**Channel has been Unlocked**');
