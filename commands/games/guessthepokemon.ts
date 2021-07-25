43c4db2485c3f81323f68edb7b3c726925283487
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-25 23:44:10 +0600

    👑GTP Done

--- commands/games/guessthepokemon.ts
+++ commands/games/guessthepokemon.ts
@@ -1,4 +1,6 @@
 import Command from '../../constants/command';
+// @ts-ignore
+import weky from 'weky'
 
 const GTPCommand: Command = {
     name: 'guessthepokemon',
@@ -13,6 +15,24 @@ const GTPCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        await weky.GuessThePokemon({
+            message: message,
+            embed: {
+                title: 'Guess The Pokémon | Falcon',
+                description:
+                    '**Type:**\n{{type}}\n\n**Abilities:**\n{{abilities}}\n\nYou only have **{{time}}** to guess the pokémon.',
+                color: '#7289da',
+                timestamp: true,
+            },
+            thinkMessage: 'I am thinking',
+            othersMessage: 'Only <@{{author}}> can use the buttons!',
+            winMessage:
+                'GG, It was a **{{answer}}**. You got it correct in **{{time}}**.',
+            loseMessage: 'Better luck next time! It was a **{{answer}}**.',
+            time: 60000,
+            incorrectMessage: "No {{author}}! The pokémon isn't `{{answer}}`",
+            buttonText: 'Cancel',
+        });
 
     },
 }
