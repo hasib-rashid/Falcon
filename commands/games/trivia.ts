1a4f17dacd905da32cb32a2806b5ff52ae03aac5
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-25 23:04:10 +0600

    👑Trivia Is Now All Done

--- commands/games/trivia.ts
+++ commands/games/trivia.ts
@@ -1,5 +1,7 @@
 import Command from '../../constants/command';
-
+// @ts-ignore
+import weky from 'weky'
+const dif = ["easy", "medium", "hard"]
 
 const TriviaCommand: Command = {
     name: 'trivia',
@@ -14,7 +16,28 @@ const TriviaCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-
+        await weky.Trivia({
+            message: message,
+            embed: {
+                title: 'Trivia | Weky Development',
+                description: 'You only have **{{time}}** to guess the answer!',
+                color: '#7289da',
+                timestamp: true,
+            },
+            difficulty: dif[Math.floor(Math.random() * dif.length)],
+            thinkMessage: '**I am thinking**',
+            winMessage:
+                '**GG, It was *{{answer}}*. You gave the correct answer in *{{time}}*.**',
+            loseMessage: '**Better luck next time! The correct answer was *{{answer}}*.**',
+            emojis: {
+                one: '1️⃣',
+                two: '2️⃣',
+                three: '3️⃣',
+                four: '4️⃣',
+            },
+            othersMessage: '**Only <@{{author}}> can use the buttons!**',
+            returnWinner: false,
+        });
     },
 }
 
