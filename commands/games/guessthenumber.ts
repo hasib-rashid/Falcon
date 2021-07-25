1f07bb083605771248760e9e77a46b62bc22099a
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-25 23:40:15 +0600

    👑GTN Done

--- commands/games/guessthenumber.ts
+++ commands/games/guessthenumber.ts
@@ -1,4 +1,6 @@
 import Command from '../../constants/command';
+// @ts-ignore
+import weky from 'weky'
 
 const GTNCommand: Command = {
     name: 'guessthenumber',
@@ -13,6 +15,34 @@ const GTNCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        await weky.GuessTheNumber({
+            message: message,
+            embed: {
+                title: 'Guess The Number | Falcon',
+                description: '**You have *{{time}}* to guess the number.**',
+                color: '#7289da',
+                timestamp: true,
+            },
+            publicGame: true,
+            number: 189,
+            time: 60000,
+            winMessage: {
+                publicGame:
+                    'GG, The number which I guessed was **{{number}}**. <@{{winner}}> made it in **{{time}}**.\n\n__**Stats of the game:**__\n**Duration**: {{time}}\n**Number of participants**: {{totalparticipants}} Participants\n**Participants**: {{participants}}',
+                privateGame:
+                    'GG, The number which I guessed was **{{number}}**. You made it in **{{time}}**.',
+            },
+            loseMessage:
+                'Better luck next time! The number which I guessed was **{{number}}**.',
+            bigNumberMessage: 'No {{author}}! My number is greater than **{{number}}**.',
+            smallNumberMessage:
+                'No {{author}}! My number is smaller than **{{number}}**.',
+            othersMessage: 'Only <@{{author}}> can use the buttons!',
+            buttonText: 'Cancel',
+            ongoingMessage:
+                "A game is already runnning in <#{{channel}}>. You can't start a new one!",
+            returnWinner: false,
+        });
 
     },
 }
