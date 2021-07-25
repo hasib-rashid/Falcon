c1b0d7710cc7ae20b0d7f0b63427b91b4c017938
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-25 23:37:57 +0600

    👑Init GTN

---
+++ commands/games/guessthenumber.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const GTNCommand: Command = {
+    name: 'guessthenumber',
+    description: 'Guess the Number',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default GTNCommand;
