fc805f3e5877b4e2406ad26a4eaa2d29770bf3fd
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-26 23:40:22 +0600

    📦Init

---
+++ commands/nsfw/thigh.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const ThighCommand: Command = {
+    name: 'thigh',
+    description: 'NSFW thigh',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: true,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default ThighCommand;
