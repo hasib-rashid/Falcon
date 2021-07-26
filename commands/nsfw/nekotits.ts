f6d838920416f5823f01a559d28c144113a56248
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-26 23:35:26 +0600

    📦Init

---
+++ commands/nsfw/nekotits.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const NekoTits: Command = {
+    name: 'nekotits',
+    description: 'NSFW nekotits',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: true,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default NekoTits;
