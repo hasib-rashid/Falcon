685fb9e19517c7e952098d1dc2939e517280f369
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-26 23:30:41 +0600

    📦Init

---
+++ commands/nsfw/hentaithigh.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const HentaiThighCommand: Command = {
+    name: 'hentaithigh',
+    description: 'NSFW hentaithigh',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: true,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default HentaiThighCommand;
