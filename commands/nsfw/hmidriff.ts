c2c6dbe7677c48fefc1060e2a56b7229af38331c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-26 23:31:54 +0600

    📦Init

---
+++ commands/nsfw/hmidriff.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const HmiDriff: Command = {
+    name: 'hmidriff',
+    description: 'NSFW hmidriff',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: true,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default HmiDriff;
