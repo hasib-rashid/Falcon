91127b9e1981614541a201cdb0900166941d5986
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-26 23:25:09 +0600

    📦Init

---
+++ commands/nsfw/anal.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const AnalCommand: Command = {
+    name: 'anal',
+    description: 'NSFW anal',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: true,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default AnalCommand;
