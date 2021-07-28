3a2ebd286bd15fa008b36a3603f28bf080d6fbef
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:27:29 +0600

    📚Init

--- commands/nsfw/hentaithigh.ts
+++ commands/nsfw/hentaithigh.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const HentaiThighCommand: Command = {
     name: 'hentaithigh',
     description: 'NSFW hentaithigh',
