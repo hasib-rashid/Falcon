65a219e7c721012ba4897c459a42fb24ae75eea7
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:27:56 +0600

    📚Init

--- commands/nsfw/hmidriff.ts
+++ commands/nsfw/hmidriff.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const HmiDriff: Command = {
     name: 'hmidriff',
     description: 'NSFW hmidriff',
