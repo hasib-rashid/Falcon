79d7bf25ff2dcd5d1beeaa5435d9e42d57c4565f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:29:26 +0600

    📚Init

--- commands/nsfw/nekopussy.ts
+++ commands/nsfw/nekopussy.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const NekoPussy: Command = {
     name: 'nekopussy',
     description: 'NSFW nekopussy',
