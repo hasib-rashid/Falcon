11e8a48ebd2a3bd83e14a4e4b749c306d6a14c9b
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:10:37 +0600

    📦Fix Import

--- commands/nsfw/kitsune.ts
+++ commands/nsfw/kitsune.ts
@@ -1,6 +1,6 @@
 import Command from '../../constants/command';
 // @ts-ignore
-import nsfw from 'discord-nsfw'
+import NSFW from 'discord-nsfw'
 
 const KitsuneCommand: Command = {
     name: 'kitsune',
