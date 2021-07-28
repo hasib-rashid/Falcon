d79ac78c082b599f479291ef31c452be24eb2077
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:25:04 +0600

    📚Init

--- commands/nsfw/fuck.ts
+++ commands/nsfw/fuck.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const FuckCommand: Command = {
     name: 'fuck',
     description: 'NSFW fuck',
