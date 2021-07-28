64535a66520dcd14136ed371a6385cf46dd36bf9
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:30:34 +0600

    📚Init

--- commands/nsfw/pgif.ts
+++ commands/nsfw/pgif.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const PGIFCommand: Command = {
     name: 'pgif',
     description: 'NSFW pgif',
