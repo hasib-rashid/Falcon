fabc70c304a85a4bbe9c3e6e49b647944accf7ff
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-28 15:23:41 +0600

    📚Init

--- commands/nsfw/boobs.ts
+++ commands/nsfw/boobs.ts
@@ -2,6 +2,8 @@ import Command from '../../constants/command';
 // @ts-ignore
 import NSFW from 'discord-nsfw'
 
+const nsfw = new NSFW()
+
 const BoobsCommand: Command = {
     name: 'boobs',
     description: 'NSFW boobs',
