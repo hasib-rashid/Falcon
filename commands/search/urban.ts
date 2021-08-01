9fedf536bc5d458885e714aeb0bb870c84d1f23d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-01 23:38:33 +0600

    🔭 Urban Init

---
+++ commands/search/urban.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const UrbanCommand: Command = {
+    name: 'urban',
+    description: 'Search Urban for all the meaning',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default UrbanCommand;
