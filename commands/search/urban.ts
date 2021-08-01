5649e707800f5f605ced6f459d7ecb626bf6eb75
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-01 23:39:22 +0600

    😶 Copy Pasta

--- commands/search/urban.ts
+++ commands/search/urban.ts
@@ -13,7 +13,39 @@ const UrbanCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-
+        try {
+            const { body } = await request
+                .get("http://api.urbandictionary.com/v0/define")
+                .query({ term: word });
+            if (!body.list.length)
+                return message.say("Could not find any results.");
+            const data = body.list[0];
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x32a8f0)
+                .setAuthor(
+                    "Urban Dictionary",
+                    "https://i.imgur.com/Fo0nRTe.png",
+                    "https://www.urbandictionary.com/"
+                )
+                .setURL(data.permalink)
+                .setTitle(data.word)
+                .setDescription(shorten(data.definition.replace(/\[|\]/g, "")))
+                .setFooter(
+                    `👍 ${formatNumber(data.thumbs_up)} 👎 ${formatNumber(
+                        data.thumbs_down
+                    )}`
+                )
+                .setTimestamp(new Date(data.written_on))
+                .addField(
+                    "❯ Example",
+                    data.example
+                        ? shorten(data.example.replace(/\[|\]/g, ""), 1000)
+                        : "None"
+                );
+            return message.embed(embed);
+        } catch (err) {
+            console.error(err);
+        }
     },
 }
 
