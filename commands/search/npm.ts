be31dda2796f6b5ef28b7818ac775ae21be04995
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-01 23:30:34 +0600

    😶 Copy pasta

--- commands/search/npm.ts
+++ commands/search/npm.ts
@@ -13,7 +13,60 @@ const NPMCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-
+        try {
+            const { body } = await request.get(
+                `https://registry.npmjs.com/${pkg}`
+            );
+            if (body.time.unpublished)
+                return msg.say("This package no longer exists.");
+            const version = body.versions[body["dist-tags"].latest];
+            const maintainers = trimArray(
+                body.maintainers.map((user) => user.name)
+            );
+            const dependencies = version.dependencies
+                ? trimArray(Object.keys(version.dependencies))
+                : null;
+            const embed = new Discord.MessageEmbed()
+                .setColor(0xcb0000)
+                .setAuthor(
+                    "NPM",
+                    "https://i.imgur.com/ErKf5Y0.png",
+                    "https://www.npmjs.com/"
+                )
+                .setTitle(body.name)
+                .setURL(`https://www.npmjs.com/package/${pkg}`)
+                .setDescription(body.description || "No description.")
+                .addField("❯ Version", body["dist-tags"].latest, true)
+                .addField("❯ License", body.license || "None", true)
+                .addField(
+                    "❯ Author",
+                    body.author ? body.author.name : "???",
+                    true
+                )
+                .addField(
+                    "❯ Creation Date",
+                    moment.utc(body.time.created).format("MM/DD/YYYY h:mm A"),
+                    true
+                )
+                .addField(
+                    "❯ Modification Date",
+                    moment.utc(body.time.modified).format("MM/DD/YYYY h:mm A"),
+                    true
+                )
+                .addField("❯ Main File", version.main || "index.js", true)
+                .addField(
+                    "❯ Dependencies",
+                    dependencies && dependencies.length
+                        ? dependencies.join(", ")
+                        : "None"
+                )
+                .addField("❯ Maintainers", maintainers.join(", "));
+            return message.embed(embed);
+        } catch (err) {
+            if (err.status === 404)
+                return msg.say("Could not find any results.");
+            console.error(err);
+        }
     },
 }
 
