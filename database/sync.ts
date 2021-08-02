746a09c1b73d744b5e7898107a869d49f7e05fcc
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-02 15:35:22 +0600

    ✅ Synced

--- database/sync.ts
+++ database/sync.ts
@@ -1,9 +1,11 @@
 import GuildUser from "../models/GuildUser"
 import MuteUser from '../models/MuteUser'
 import BlackList from '../models/BlackListUsers'
+import GuildModel from '../models/GuildModel'
 
 export function Sync() {
     GuildUser.sync({ logging: false })
     MuteUser.sync({ logging: false })
     BlackList.sync({ logging: false })
+    GuildModel.sync({ logging: false })
 }
