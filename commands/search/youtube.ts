b7daad611abf138cecd4d890a968c8a4eac20a85
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-02 12:55:57 +0600

    📰Done

--- commands/search/youtube.ts
+++ commands/search/youtube.ts
@@ -1,4 +1,6 @@
 import Command from '../../constants/command';
+import { default as axios } from 'axios'
+import { MessageEmbed } from 'discord.js'
 
 const YoutubeCommand: Command = {
     name: 'youtube',
@@ -13,7 +15,58 @@ const YoutubeCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
+        const options = {
+            method: "GET",
+            url: "https://youtube-v31.p.rapidapi.com/search",
+            params: {
+                q: "microsoft",
+                part: "snippet,id",
+                regionCode: "US",
+                maxResults: "5",
+                order: "date",
+            },
+            headers: {
+                "x-rapidapi-key": process.env.API_KEY,
+                "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+            },
+        };
 
+        axios
+            .get("https://youtube-v31.p.rapidapi.com/search", {
+                method: "GET",
+                params: {
+                    q: args.join(" "),
+                    part: "snippet,id",
+                    maxResults: "7",
+                    order: "date",
+                },
+                headers: {
+                    "x-rapidapi-key": process.env.RAPID_API_KEY,
+                    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+                },
+            })
+            .then(function (response) {
+                console.log(response.data)
+                const embed = new MessageEmbed()
+                    .setAuthor(
+                        "Youtube",
+                        "https://www.freepnglogos.com/uploads/youtube-logo-icon-transparent---32.png"
+                    )
+                    .setColor("#fa2f44");
+                for (var i = 0; i < response.data.items.length; ++i) {
+                    var result = response.data.items[i];
+
+                    embed.addField(
+                        `**${result.snippet.channelTitle}** - ${result.snippet.title}`,
+                        `[Link](https://youtube.com/watch?v=${result.id.videoId}), [Channel Link](https://youtube.com/channel/${result.snippet.channelId}) - ${result.snippet.description}`
+                    );
+                }
+
+                message.channel.send(embed);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     },
 }
 
