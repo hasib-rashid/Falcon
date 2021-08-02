e8700601b9d20dfbe1e8d671e0c5d037280f8067
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-02 15:22:47 +0600

    👑 Dynamic Prefix is Done

--- events/message.ts
+++ events/message.ts
@@ -7,6 +7,7 @@ import Event from "../constants/event";
 import { stripIndents } from "common-tags";
 import Collection from "@discordjs/collection";
 import BlackList from '../models/BlackListUsers'
+import GuildModel from '../models/GuildModel'
 
 Nuggies.connect(process.env.MONGO_URL)
 
@@ -18,72 +19,72 @@ const MessageEvent: Event = {
     async run(client, message: Message) {
         if (message.author.bot || message.webhookID) return;
 
-        // Will keep plugins here
-        const prefix = "." //TODO Dynamic Prefix
-
-        if (message.content === prefix) return;
-
-        if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
-
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-        const commandName = args.shift()?.toLowerCase();
-
-        // @ts-ignore
-        const command = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
-
-        if (!command) return;
-
-        if (message.content === `${prefix}${command.name}`) {
+        GuildModel.findOne({ where: { guildID: message.guild?.id } }).then((response) => {
             // @ts-ignore
-            BlackList.findOne({ where: { userID: message.author.id } }).then((response: any, err: any) => {
-                if (err) return;
-                if (message.author.id === response.dataValues.userID) return message.channel.send(`**${message.author} you have been blacklisted by the owner. Check your dm's to see how to get whitelisted again**`)
-            })
-        }
+            const prefix = response.dataValues.prefix || "."
 
-        if (command.guildOnly && !message.guild) return message.channel.send(`${client.emotes.error} This command can only be used in Servers!`);
+            if (message.content === prefix) return;
 
-        if (command.ownerOnly && message.author.id !== client.ownerID) return message.channel.send(`**${client.emotes.error} To use this command, you need to be as **crazy** and **nerdy** as my Owner**`);
+            if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
 
-        if (command.disabled) return message.channel.send(`**${client.emotes.error} The command has been disabled by the owner**`);
+            const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
-        // @ts-ignore
-        if (command.nsfw && (!message.guild || !message.channel.nsfw)) return message.channel.send(`**${client.emotes.error} This command can only be used in a NSFW Channel.**`);
-
-        if (!!command.args && !args[command.args - 1]) return message.channel.send(`${client.emotes.error} Expected ${command.args} arguments, received ${args.length}`);
-        if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Collection());
-        }
-
-        const now = Date.now();
-        const timestamps = cooldowns.get(command.name);
-        const cooldownAmount = (command.cooldown || 3) * 1000;
-
-        if (timestamps?.has(message.author.id)) {
-            // @ts-ignore
-            const expirationStamp = timestamps.get(message.author.id) + cooldownAmount;
+            const commandName = args.shift()?.toLowerCase();
 
             // @ts-ignore
-            if (now < expirationStamp) return message.channel.send(`Please wait for ${((expirationStamp - now) / 1000).toFixed(2)} seconds before trying to use the command again.`);
-        }
+            const command = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
 
-        timestamps?.set(message.author.id, now);
+            if (!command) return;
 
-        setTimeout(() => timestamps?.delete(message.author.id), cooldownAmount);
+            if (message.content === `${prefix}${command.name}`) {
+                // @ts-ignore
+                BlackList.findOne({ where: { userID: message.author.id } }).then((response: any, err: any) => {
+                    if (err) return;
+                    if (message.author.id === response.dataValues.userID) return message.channel.send(`**${message.author} you have been blacklisted by the owner. Check your dm's to see how to get whitelisted again**`)
+                })
+            }
 
-        try {
-            command.run(client, message, args);
-        }
-        catch (err) {
-            client.logger.error("client/commands", stripIndents(`
+            if (command.guildOnly && !message.guild) return message.channel.send(`${client.emotes.error} This command can only be used in Servers!`);
+
+            if (command.ownerOnly && message.author.id !== client.ownerID) return message.channel.send(`**${client.emotes.error} To use this command, you need to be as **crazy** and **nerdy** as my Owner**`);
+
+            if (command.disabled) return message.channel.send(`**${client.emotes.error} The command has been disabled by the owner**`);
+
+            // @ts-ignore
+            if (command.nsfw && (!message.guild || !message.channel.nsfw)) return message.channel.send(`**${client.emotes.error} This command can only be used in a NSFW Channel.**`);
+
+            if (!!command.args && !args[command.args - 1]) return message.channel.send(`${client.emotes.error} Expected ${command.args} arguments, received ${args.length}`);
+            if (!cooldowns.has(command.name)) {
+                cooldowns.set(command.name, new Collection());
+            }
+
+            const now = Date.now();
+            const timestamps = cooldowns.get(command.name);
+            const cooldownAmount = (command.cooldown || 3) * 1000;
+
+            if (timestamps?.has(message.author.id)) {
+                // @ts-ignore
+                const expirationStamp = timestamps.get(message.author.id) + cooldownAmount;
+
+                // @ts-ignore
+                if (now < expirationStamp) return message.channel.send(`Please wait for ${((expirationStamp - now) / 1000).toFixed(2)} seconds before trying to use the command again.`);
+            }
+
+            timestamps?.set(message.author.id, now);
+
+            setTimeout(() => timestamps?.delete(message.author.id), cooldownAmount);
+
+            try {
+                command.run(client, message, args);
+            }
+            catch (err) {
+                client.logger.error("client/commands", stripIndents(`
 				Command Name: ${command.name}
 
 				Error: ${err.message}
 			`));
-        }
-
-
+            }
+        })
     },
 };
 
