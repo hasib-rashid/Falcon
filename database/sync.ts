b242239852b48cca594795bd7ef47379a40c3d24
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-03 22:56:34 +0600

    🔭 Fixed the Sync

--- database/sync.ts
+++ database/sync.ts
@@ -1,10 +1,8 @@
-import GuildUser from "../models/GuildUser"
 import MuteUser from '../models/MuteUser'
 import BlackList from '../models/BlackListUsers'
 import GuildModel from '../models/GuildModel'
 
 export function Sync() {
-    GuildUser.sync({ logging: false })
     MuteUser.sync({ logging: false })
     BlackList.sync({ logging: false })
     GuildModel.sync({ logging: false })
