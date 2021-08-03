5b93fe326e57dcf7a11cb20770ea0fb40b73a83d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-03 23:01:45 +0600

    🎶 Added Lyrics

---
+++ commands/music/lyrics.ts
@@ -0,0 +1,20 @@
+import Command from '../../constants/command';
+
+const LyricsCommand: Command = {
+    name: 'lyrics',
+    description: 'Look at the lyrics of Falcon',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+
+    },
+}
+
+export default LyricsCommand;
