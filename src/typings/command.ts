4307a28f65886025ca15d48e92568dc5e452786a
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-08 22:55:12 +0600

    👑 Command Interface Done

---
+++ src/typings/command.ts
@@ -0,0 +1,18 @@
+import { Message } from 'discord.js';
+import { Falcon } from '../classes/Client';
+
+export interface RunFunction {
+    (client: Falcon, message: Message, args: string[]): Promise<unknown>;
+}
+
+export interface Command {
+    name: string;
+    run: RunFunction;
+    aliases?: string[];
+    description: string;
+    cooldown?: number;
+    category: string;
+    userPermissions?: string | string[];
+    ownerOnly?: boolean;
+    usage?: string;
+}
