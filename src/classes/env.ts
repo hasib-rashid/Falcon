f44a01d5aff33d66beb764e49146ee7fe7464e96
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 15:24:50 +0600

    🔧 ENV

--- src/classes/env.ts
+++ src/classes/env.ts
@@ -8,8 +8,5 @@ export const ENV: Environment = {
     google_api_key: process.env.GOOGLE_API_KEY,
     giphy_api_key: process.env.GIPHY_API_KEY,
     rapid_api_key: process.env.RAPID_API_KEY,
-    db_username: process.env.DB_USERNAME,
-    db_name: process.env.DB_NAME,
-    db_password: process.env.DB_PASSWORD,
-    db_hostname: process.env.DB_HOSTNAME
+    db: process.env.DB
 }
