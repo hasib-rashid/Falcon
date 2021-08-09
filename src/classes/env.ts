0f908ff48afc03014df99cf32bb80bd67ee57f86
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 09:01:08 +0600

    📚 Environment fully done

--- src/classes/env.ts
+++ src/classes/env.ts
@@ -1,6 +1,15 @@
 import { config } from 'dotenv'
+import Environment from '../typings/env'
 config()
 
-export const ENV = {
-
+export const ENV: Environment = {
+    token: process.env.TOKEN,
+    mongoURL: process.env.MONGO_URL,
+    google_api_key: process.env.GOOGLE_API_KEY,
+    giphy_api_key: process.env.GIPHY_API_KEY,
+    rapid_api_key: process.env.RAPID_API_KEY,
+    db_username: process.env.DB_USERNAME,
+    db_name: process.env.DB_NAME,
+    db_password: process.env.DB_PASSWORD,
+    db_hostname: process.env.DB_HOSTNAME
 }
