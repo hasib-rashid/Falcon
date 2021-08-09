71bc833e431a0def6eeeb590527b9b1dba533296
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 10:45:14 +0600

    😶 Init

---
+++ src/commands/general/avatar.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../typings/command';
+
+export const name: string = 'avatar';
+export const category: string = 'general';
+export const description: string = 'Check your avatar';
+
+export const run: RunFunction = async (client, message) => {
+
+}
