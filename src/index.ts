497398622a817154966cebe69d7b625c66359ff0
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 09:06:28 +0600

    👑 Index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { config } from 'dotenv'
-config()
-
 import { Falcon } from './classes/client'
+import { ENV } from './classes/env'
 
 new Falcon().start({
-    token: process.env.TOKEN,
+    token: ENV.token,
     owners: ["548038495617417226"],
     prefix: ".",
     mongoURI: ""
