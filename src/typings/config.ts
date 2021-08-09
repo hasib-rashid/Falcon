c1836601e5880e97d63d9ad2cd71d82ec499e843
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 12:30:46 +0600

    😑 Still fixing

--- src/typings/config.ts
+++ src/typings/config.ts
@@ -1,10 +1,16 @@
-export interface Config {
-    token: string;
-    mongoURI: string;
-    prefix: string;
-    owners: Array<string>;
-    webAuth?: string;
-    webPort?: number;
-    topGGToken?: string;
-    onlyUsed?: string[];
-}
+export default interface Config {
+    token: string | undefined,
+    ownerID: string,
+    prefix?: string,
+    distube?: any,
+    commandDir: string,
+    eventDir: string,
+    emotes: {
+        success: string,
+        error: string,
+        loading: string,
+        bot: string,
+        chat: string,
+    }
+    // eslint-disable-next-line semi
+};
