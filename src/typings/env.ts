cc9d32efe63aa31ec03040486b0662c49b4c3998
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-09 08:58:28 +0600

    👑 Environment Variables Done

---
+++ src/typings/env.ts
@@ -0,0 +1,11 @@
+export default interface Environment {
+    token: string;
+    mongoURL: string;
+    google_api_key: string;
+    giphy_api_key: string;
+    rapid_api_key: string;
+    db_username: string;
+    db_name: string;
+    db_password: string;
+    db_hostname: string;
+}
