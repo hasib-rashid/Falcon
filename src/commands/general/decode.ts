9c96b7ee0aa46f7aa4cf5517d884b9c6cbbf49aa
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-11 13:29:57 +0600

    ❤Decode Done

---
+++ src/commands/general/decode.ts
@@ -0,0 +1,44 @@
+import Command from '../../typings/command';
+import { MessageEmbed } from 'discord.js'
+import { default as axios } from 'axios'
+
+const DecodeCommand: Command = {
+    name: 'decode',
+    description: 'Use this command to decode binary',
+    aliases: [
+        ''
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        if (!args.join(" "))
+            return message.channel.send(
+                "**Please send a Binary Code to Decode**"
+            );
+
+        try {
+            axios.get(`http://some-random-api.ml/binary?decode=${args.join(" ")}`).then(function (response) {
+                const embed = new MessageEmbed()
+                    .setAuthor(
+                        message.author.username,
+                        message.author.displayAvatarURL()
+                    )
+                    .setTitle("Decoding Binary")
+                    .setColor("#118eed")
+                    .setDescription(response.data.text);
+
+                message.channel.send({ embeds: [embed] });
+            });
+        } catch (e) {
+            return message.channel.send(
+                `**An error occured, please try again and put binary codes this time.**`
+            );
+        }
+    },
+}
+
+export default DecodeCommand;
