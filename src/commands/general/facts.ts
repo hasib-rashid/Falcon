f314e870ffe59d2d430a911dd50395f29a1b0ca3
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-11 13:30:46 +0600

    ❤Decode Done

---
+++ src/commands/general/facts.ts
@@ -0,0 +1,23 @@
+import Command from '../../typings/command';
+import { default as axios } from 'axios'
+
+const FactsCommand: Command = {
+    name: 'fact',
+    description: 'Get a random fact!',
+    aliases: [
+        'facts'
+    ],
+    guildOnly: false,
+    ownerOnly: false,
+    disabled: false,
+    nsfw: false,
+    cooldown: 0,
+
+    async run(client, message, args) {
+        axios.get("https://uselessfacts.jsph.pl/random.json?language=en").then((response) => {
+            message.channel.send(`**${response.data.text}**`)
+        })
+    },
+}
+
+export default FactsCommand;
