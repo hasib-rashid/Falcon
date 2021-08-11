1fd7d39bea1dfb09810bd8f280302f8e989e2fdd
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-11 15:51:30 +0600

    🔧Fix

--- src/commands/admin/unlock.ts
+++ src/commands/admin/unlock.ts
@@ -13,7 +13,7 @@ const UnlockCommand: Command = {
     cooldown: 0,
 
     async run(client, message, args) {
-        if (!message.member?.hasPermission("MANAGE_CHANNELS"))
+        if (!message.member?.permissions.has("MANAGE_CHANNELS"))
             return message.channel.send(
                 "**You need `MANAGE_CHANNELS` permission to use this command**"
             );
