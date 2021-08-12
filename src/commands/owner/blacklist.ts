62080debc6751e0e77c5f2e2ccddba4ee4956241
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-12 10:56:14 +0600

    🎉Blacklisted Command Done

--- src/commands/owner/blacklist.ts
+++ src/commands/owner/blacklist.ts
@@ -1,5 +1,8 @@
 import Command from '../../typings/command';
-import BlackList from '../../models/BlackListUsers'
+import { Deta } from 'deta'
+import { ENV } from '../../classes/env';
+const deta = Deta(ENV.db)
+const db = deta.Base("blacklist")
 
 const BlackListCommand: Command = {
     name: 'blacklist',
@@ -17,8 +20,12 @@ const BlackListCommand: Command = {
         const user = client.users.cache.get(args[0])
         const reason = args.slice(1).join(' ') || "No Reason";
 
-        BlackList.create({ userID: args[0], time: Date.now(), reason: reason })
-        message.channel.send(`${user?.username} was successfully Blacklisted.`)
+        try {
+            db.put({ userID: user.id, time: Date.now(), reason: reason })
+            message.channel.send("**Sucessfully Blacklisted this user**")
+        } catch (err) {
+            message.channel.send("**This user is already Blacklisted**")
+        }
     },
 }
 
