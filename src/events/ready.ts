2396fa707dba24b36a95c7aa96faac364e484226
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-12 14:00:28 +0600

    👑Done the Muting System

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,11 @@ import consola from 'consola'
 import Event from '../typings/event'
 import Nuggies from 'nuggies'
 require("dotenv").config();
+import ms from 'ms'
+import { Deta } from 'deta'
+import { ENV } from '../classes/env';
+const deta = Deta(ENV.db)
+const db = deta.Base("muted")
 
 const ReadyEvent: Event = {
     name: "ready",
@@ -23,6 +28,22 @@ const ReadyEvent: Event = {
         }, 10000);
 
         Nuggies.giveaways.startAgain(client);
+
+        client.guilds.cache.map((ev) => {
+            ev.members.cache.map((member) => {
+                // @ts-ignore
+                if (member.roles.cache.has(ev?.roles.cache.find(r => r.name.toLowerCase() === 'muted')?.id)) {
+                    db.fetch({ userID: member.id, guildID: member.guild.id }).then((response) => {
+                        setInterval(async () => {
+                            // @ts-ignore
+                            await member.roles.remove(member.guild.roles.cache.find((role) => role.name.toLowerCase() === "muted"))
+
+                            db.delete(response.items[0].key as any)
+                        }, ms(response.items[0].time))
+                    })
+                }
+            })
+        })
     },
 };
 
