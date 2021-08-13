e126d31433e0256d01db27258b6778ca174eae6e
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-13 23:26:00 +0600

    ✅Create Info

---
+++ src/commands/misc/info.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'info'
+export const category = 'misc'
+export const description = 'Information about Falcon'
+
+export const run: RunFunction = async (client, message, args) => {
+     
+}
