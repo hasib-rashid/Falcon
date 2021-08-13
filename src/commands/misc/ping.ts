d4d996ca571dd2382a8926a42a7cfb0d1b96a959
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-13 23:23:07 +0600

    ✅Ping Done

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -1,9 +1,28 @@
+import { MessageEmbed } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command'; 
 
 export const name = 'ping'
 export const category = 'misc'
-export const description = 'Description'
+export const description = 'Check the Ping of Falcon'
 
 export const run: RunFunction = async (client, message, args) => {
-   message.channel.send(client.ws.ping)
+	message.channel.send("Loading data! :thinking:").then(async (msg) => {
+		const pingEmbed = new MessageEmbed()
+			.setTitle("Ping")
+			.setAuthor(`Requested by ${message.author.tag}`)
+			.setDescription(
+				`**🏓 Pong! Your Latency is \`${msg.createdTimestamp - message.createdTimestamp
+				}ms\` and API Latency is \`${Math.round(
+					client.ws.ping
+				)}\` ms!**`
+			);
+
+		if (client.ws.ping < 120) pingEmbed.setColor("GREEN");
+
+		if (client.ws.ping < 500) pingEmbed.setColor("YELLOW");
+
+		if (client.ws.ping > 500) pingEmbed.setColor("RED");
+
+		message.channel.send(pingEmbed)
+	}); 
 }
