adf4297341eeb310c13ab4ecef88be7bd7496230
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-14 15:54:34 +0600

    ❤Drop Done

---
+++ src/commands/admin/drop.ts
@@ -0,0 +1,38 @@
+import { RunFunction } from '../../interfaces/Command';
+import Nuggies from 'nuggies'
+
+export const name = 'drop'
+export const category = 'admin'
+export const description = 'Drop a item in Discord'
+
+export const run: RunFunction = async (client, message, args) => {
+    let prize
+    let channel
+
+    message.channel.send("**What should be the prize of the giveaway. You have 30 seconds to answer this.**")
+    await message.channel.awaitMessages(m => m.author.id == message.author.id,
+        { max: 1, time: 30000 }).then(collected => {
+            prize = collected.first()?.content
+        }).catch(() => {
+            message.reply('**No answer after 30 seconds, operation canceled.**');
+        })
+
+    message.channel.send("**What is the channel this giveaway will happen? Type 'here' to do the giveaway here. You have 30 seconds to answer this.**")
+    await message.channel.awaitMessages(m => m.author.id == message.author.id,
+        { max: 1, time: 30000 }).then(collected => {
+            if (collected.first()?.content === "here") {
+                channel = message.channel.id
+            } else {
+                channel = collected.first()?.content
+            }
+        }).catch(() => {
+            message.reply('**No answer after 30 seconds, operation canceled.**');
+        })
+
+    Nuggies.giveaways.drop({
+        message: message,
+        prize: prize,
+        host: message.author.id,
+        channel: channel,
+    });
+}
