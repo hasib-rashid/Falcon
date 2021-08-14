f0dfadc50070ca931082f293b9824ba56167c6c8
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-14 15:50:57 +0600

    ❤DeleteChannel also Done

---
+++ src/commands/admin/deletechannel.ts
@@ -0,0 +1,20 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'deletechannel'
+export const category = 'admin'
+export const description = 'Delete a channel'
+
+export const run: RunFunction = async (client, message, args) => {
+    if (!message.member?.hasPermission("MANAGE_CHANNELS"))
+        return message.channel.send(
+            "**You need `MANAGE_CHANNELS` permission to use this command**"
+        );
+    const channel = message.mentions.channels.first() || message.guild?.channels.cache.get(args[0])
+
+    if (!channel) return message.channel.send("**Please mention a channel to delete**")
+
+    // @ts-ignore
+    message.guild?.channels.cache.get(channel?.id)?.delete()
+
+    message.channel.send("**Successfully Deleted the Channel**")
+}
