21902e8102021bcc27ac560b6a9550b28466ecfb
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-15 22:45:42 +0600

    📚Blacklist Done

---
+++ src/commands/owner/blacklist.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+config()
+
+import { RunFunction } from '../../interfaces/Command';
+import { Deta } from 'deta'
+const deta = Deta(process.env.DEFAULT_DB)
+const db = deta.Base("blacklist")
+
+export const name = 'blacklist'
+export const category = 'owner'
+export const description = 'Blacklist Someone in Discord'
+
+export const run: RunFunction = async (client, message, args) => {
+    const user = client.users.cache.get(args[0])
+    const reason = args.slice(1).join(' ') || "No Reason";
+
+    try {
+        db.put({ userID: user.id, time: Date.now(), reason: reason, condition: true })
+        message.channel.send("**Sucessfully Blacklisted this user**")
+    } catch (err) {
+        message.channel.send("**This user is already Blacklisted**")
+    }
+}
