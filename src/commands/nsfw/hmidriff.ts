c7a5c9dd8159cd8ce8045143bc7a32ca33450516
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-15 23:26:20 +0600

    📦Hmidriff

---
+++ src/commands/nsfw/hmidriff.ts
@@ -0,0 +1,11 @@
+import { RunFunction } from '../../interfaces/Command';
+import NSFW from 'discord-nsfw'
+const nsfw = new NSFW();
+
+export const name = 'hmidriff'
+export const category = 'nsfw'
+export const description = 'NSFW Hmidriff'
+
+export const run: RunFunction = async (client, message, args) => {
+    message.channel.send(await nsfw.hmidriff())
+}
