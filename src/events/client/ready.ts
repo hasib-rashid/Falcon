71de3f3c45c94818c4e41d7776ee0169e354cf80
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-15 15:41:15 +0600

    ❤Done Muted

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,7 +1,15 @@
+import { config } from 'dotenv';
+config()
+
 import { RunFunction } from '../../interfaces/Event';
 import { GuildMember } from 'discord.js';
 import express from 'express';
 import Nuggies from 'nuggies'
+
+import { Deta } from 'deta'
+import ms from 'ms';
+const deta = Deta(process.env.DEFAULT_DB)
+const db = deta.Base("muted")
 
 export const name: string = 'ready';
 export const run: RunFunction = async (client) => {
@@ -19,6 +27,22 @@ export const run: RunFunction = async (client) => {
 	}
 
 	Nuggies.giveaways.startAgain(client);
+
+	client.guilds.cache.map((ev) => {
+		ev.members.cache.map((member) => {
+			// @ts-ignore
+			if (member.roles.cache.has(ev?.roles.cache.find(r => r.name.toLowerCase() === 'muted')?.id)) {
+				db.fetch({ userID: member.id, guildID: member.guild.id }).then((response) => {
+					setInterval(async () => {
+						// @ts-ignore
+						await member.roles.remove(member.guild.roles.cache.find((role) => role.name.toLowerCase() === "muted"))
+
+						db.delete(response.items[0].key as any)
+					}, ms(response.items[0].time as string))
+				})
+			}
+		})
+	})
 
 	const app = express();
 	app.get('/', (req, res) => res.status(200).json({ msg: '🚀' }));
