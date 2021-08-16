104a354e32531a36ca8b119e180effac1b8e0be5
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-16 06:12:03 +0600

    👑Create

---
+++ src/commands/notify/bug.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'name'
+export const category = 'category'
+export const description = 'description'
+
+export const run: RunFunction = async (client, message, args) => {
+    // Code here! 
+}
