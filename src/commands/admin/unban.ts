7b55d135dcfdbaa60d0c4524bcf7c7dfa9c1761d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-23 13:24:33 +0600

    ✅Target Users

--- src/commands/admin/unban.ts
+++ src/commands/admin/unban.ts
@@ -5,5 +5,7 @@ export const category = 'admin'
 export const description = 'Unban a banned user'
 
 export const run: RunFunction = async (client, message, args) => {
+    const targetUser = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
+
 
 }
