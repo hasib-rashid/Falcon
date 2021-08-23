a0c44c94656e255253a1c7dae6a227aa7ea17b88
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-23 12:36:24 +0600

    ✅Uncomment

--- src/commands/admin/ban.ts
+++ src/commands/admin/ban.ts
@@ -52,7 +52,7 @@ export const run: RunFunction = async (client, message, args) => {
         if (button.id === "ban-yes") {
             if (!button.message.author) return;
 
-            // targetUser?.ban({ reason: banReason })
+            targetUser?.ban({ reason: banReason })
 
             banMessage.then((msg: Message) => {
                 msg.delete()
