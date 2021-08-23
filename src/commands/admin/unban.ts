a3cd454ee718728e673d5f640db9a5e63dfea0c3
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-23 13:33:35 +0600

    ✅Unban Done

--- src/commands/admin/unban.ts
+++ src/commands/admin/unban.ts
@@ -7,5 +7,15 @@ export const description = 'Unban a banned user'
 export const run: RunFunction = async (client, message, args) => {
     const targetUser = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
 
+    message.guild.fetchBans().then((bans) => {
+        if (bans.size === 0) return message.channel.send("**There is no users banned**")
 
+        const banUser = bans.find((b) => b.user.id === targetUser.id)
+
+        if (!banUser) return message.channel.send("**This user is not banned**")
+
+        message.guild.members.unban(banUser.user)
+
+        message.channel.send("**Successfully Unbanned the user**")
+    })
 }
