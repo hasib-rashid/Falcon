870e36fcc817388e1b8141070b0c5f203dd38402
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-23 12:46:27 +0600

    ✅Unban Done

---
+++ src/commands/admin/unban.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'unban'
+export const category = 'admin'
+export const description = 'Unban a banned user'
+
+export const run: RunFunction = async (client, message, args) => {
+
+}
