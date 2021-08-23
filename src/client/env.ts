ca7df2fba9a847bef520409decd4001c32ecfb17
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-23 13:47:04 +0600

    📚Environment is Done

--- src/client/env.ts
+++ src/client/env.ts
@@ -1,4 +1,13 @@
+import { config } from 'dotenv'
+config()
+
 import { ENV } from "../interfaces/Env";
 
 export const env: ENV = {
+    token: process.env.TOKEN,
+    prefix: process.env.PREFIX,
+    rapid_api: process.env.RAPID_API_KEY,
+    google_api: process.env.GOOGLE_API_KEY,
+    giphy_api: process.env.GIPHY_API_KEY,
+    db: process.env.DEFAULT_DB
 }
