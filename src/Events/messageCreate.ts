5ecb5285cfe8ca3afdb4dad96c41525daecd534c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-26 18:25:09 +0600

    🚀Message Event

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -1,4 +1,5 @@
-import { CommandInteraction, Interaction, Message } from "discord.js";
+import { CommandInteraction, Interaction, Message, TextChannel } from "discord.js";
+import consola from 'consola'
 import BaseEvent from "../base/BaseEvent";
 import CodeFictionist from "../base/Client";
 
@@ -8,13 +9,50 @@ export default class MessageCreateEvent extends BaseEvent {
     }
 
     public async run(message: Message) {
+        try {
+            if (message.partial) await message.fetch();
+            if (message.member?.partial) await message.member.fetch();
+            if (!message.guild) return;
+            if (message.author.bot) return;
+
+            const Prefix = "."
+
+            if (!message.content.toLowerCase().startsWith(Prefix)) return;
+            const [cmd, ...args]: string[] = message.content
+                .slice(Prefix.length)
+                .trim()
+                .split(/ +/g);
+
+            const command: any = this.client.commands.get(cmd.toLowerCase())
+
+            if (!cmd.length) return;
+            if (command) {
+                command.run(this.client, message, args).catch((e: Error) => {
+                    consola.error(e);
+                    message.channel
+                        .send(
+                            "Error Occured"
+                        )
+                        .catch(() => consola.error("Can't send error message"));
+                    if (e?.message?.toLowerCase()?.includes('missing permissions') || false)
+                        return;
+                    return (
+                        this.client.channels.cache.get('875785159331434607') as TextChannel
+                    ).send(
+                        "Error Occured"
+                    );
+                });
+            }
+        } catch (err) {
+
+        }
     }
 
     private async __handleCommand(message: Message) {
         try {
         }
         catch (err) {
-            this.client.logger.error("client/commands", (err as Error).message, (err as Error).stack);
+            this.client.logger.error("this.client/commands", (err as Error).message, (err as Error).stack);
         }
     }
 }
