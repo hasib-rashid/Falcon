7145a045d7c2136222b0411a296f9a625a917a11
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 07:28:44 +0600

    🚀Tempban Fix

--- src/commands/admin/tempban.ts
+++ src/commands/admin/tempban.ts
@@ -1,20 +1,24 @@
-import { MessageActionRow, MessageButton } from 'discord-buttons';
-import { MessageEmbed, PermissionResolvable } from 'discord.js';
+import { Message, GuildMember, MessageActionRow, MessageButton, MessageComponentInteraction, MessageEmbed, PermissionResolvable } from 'discord.js';
 import { RunFunction } from '../../interfaces/Command';
 
 export const name = 'tempban'
 export const category = 'admin'
-export const description = 'Temporarily Ban someone'
+export const description = 'Temporarily Ban a User'
 export const userPermissions: PermissionResolvable = "BAN_MEMBERS"
 
 export const run: RunFunction = async (client, message, args) => {
     if (!message.author) return;
 
-    const banReason = args.slice(2).join(' ') || "No Reason";
+    if (!message.member.permissions.has("BAN_MEMBERS"))
+        return message.channel.send(
+            "**You need `BAN_MEMBERS` permission to use this command**"
+        );
 
+    const banReason = args.slice(2).join(' ') ? args.slice(1).join(' ') : "No Reason";
     const banTime = args[2]
 
-    const targetUser = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
+    const targetUser = message.mentions.
+        members?.first() || message.guild?.members.cache.get(args[0])
 
     // @ts-ignore
     if (!message.guild?.member(targetUser)?.bannable) return message.channel.send("**Could not ban this user due to role hierchy**");
@@ -30,43 +34,55 @@ export const run: RunFunction = async (client, message, args) => {
         .setFooter(message.client.user?.username, message.client.user?.displayAvatarURL())
 
     const confirmButton = new MessageButton()
+        .setCustomId("ban-yes")
         .setLabel("Yes")
-        .setID("ban-yes")
-        .setStyle("green");
-
+        .setStyle("SUCCESS")
 
     const denyButton = new MessageButton()
+        .setCustomId("ban-no")
         .setLabel("No")
-        .setID("ban-no")
-        .setStyle("red");
+        .setStyle("DANGER")
 
     const row = new MessageActionRow()
         .addComponents(confirmButton, denyButton)
 
-    message.channel.send(confirmEmbed, row)
+    const banYesFilter = (i: MessageComponentInteraction) => i.customId === 'ban-yes'
+    const banYesCollector = message.channel.createMessageComponentCollector({ filter: banYesFilter, time: 30000 });
 
-    client.on('clickButton', async (button) => {
-        if (button.id === "ban-yes") {
-            if (!button.message.author) return;
+    const banNoFilter = (i: MessageComponentInteraction) => i.customId === 'ban-no'
+    const banNoCollector = message.channel.createMessageComponentCollector({ filter: banNoFilter, time: 30000 });
 
-            targetUser?.ban({ reason: banReason, days: +banTime })
+    message.reply({ embeds: [confirmEmbed], components: [row] })
 
-            message.channel.send(`**Successfully Banned ${targetUser} from this server Temporarily.**`)
+    banYesCollector.on('collect', async (i: MessageComponentInteraction) => {
+        if (i.customId === 'ban-yes') {
+            if (i.user.id !== message.author.id) {
+                i.reply({ content: "**You did not send this command. So you cannot use it unless you send the command yourself**", ephemeral: true })
+            } else {
+                message.edit({ content: `**Successfully Banned ${targetUser} for \`${banReason}\`**`, embeds: [], components: [] })
 
-            const banEmbed = new MessageEmbed()
-                .setAuthor(message.author.username, message.author.displayAvatarURL())
-                .setTitle(`Temporariloy Banned from ${message.guild?.name}`)
-                .setDescription(`**${message.author} Has Banned you temporarily from ${message.guild?.name} for \`${banReason}\` For \`${banTime} Days\`. Please contact him if you want to get unbanned.**`)
-                .setColor("#ed3737")
-                .setFooter(client.user?.username, client.user?.displayAvatarURL())
+                const banEmbed = new MessageEmbed()
+                    .setAuthor(message.author.username, message.author.displayAvatarURL())
+                    .setTitle(`Banned from ${message.guild?.name}`)
+                    .setDescription(`**${message.author} Has Banned you from ${message.guild?.name} for \`${banReason}\`. Please contact him if you want to get unbanned.**`)
+                    .setColor("#ed3737")
+                    .setFooter(client.user?.username, client.user?.displayAvatarURL())
 
-            targetUser?.send(banEmbed).catch((err) => { message.channel.send("**Message wasn't sent to this user because this user has his DM's disabled.**") })
-        }
-
-        if (button.id === "ban-no") {
-            if (!button.message.author) return;
-
-            button.message.channel.send("**Canceled The Action.**")
+                await targetUser?.send({ embeds: [banEmbed] }).catch((err) => { i.channel.send("**Message wasn't sent to this user because this user has his DM's disabled.**") })
+                // targetUser.ban({ reason: `${banReason}`, days: +banTime })
+            }
         }
     });
+
+    banNoCollector.on('collect', async (i: MessageComponentInteraction) => {
+        if (i.customId === 'ban-no') {
+            if (i.user.id !== message.author.id) {
+                i.reply({ content: "**You did not send this command. So you cannot use it unless you send the command yourself**", ephemeral: true })
+            } else {
+                message.reply({ content: "**Cancelled the action**", components: [] })
+            }
+        }
+    });
+
+    return;
 }
