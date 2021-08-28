84e0272cd233a7b577ce5977faefb2d18addf88d
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 07:31:38 +0600

    🚀Unban Fix

--- src/commands/admin/unban.ts
+++ src/commands/admin/unban.ts
@@ -5,9 +5,12 @@ export const category = 'admin'
 export const description = 'Unban a banned user'
 
 export const run: RunFunction = async (client, message, args) => {
-    const targetUser = message.mentions.members?.first() || message.guild?.members.cache.get(args[0])
+    if (!message.member.permissions.has("BAN_MEMBERS"))
+        return message.channel.send(
+            "**You need `BAN_MEMBERS` permission to use this command**"
+        );
 
-    message.guild.fetchBans().then((bans) => {
+    message.guild.bans.fetch().then((bans) => {
         if (bans.size === 0) return message.channel.send("**There is no users banned**")
 
         const banUser = bans.find((b) => b.user.id === args[0])
