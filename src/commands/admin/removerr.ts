3d97374aa22f0ede9eb5de5d82bb4f49c92d9d1c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 07:20:12 +0600

    🚀RemoveRR Done

--- src/commands/admin/removerr.ts
+++ src/commands/admin/removerr.ts
@@ -8,7 +8,7 @@ export const userPermissions: PermissionResolvable = "MANAGE_MESSAGES"
 
 export const run: RunFunction = async (client, message, args) => {
     try {
-        if (!message.member?.hasPermission("MANAGE_ROLES"))
+        if (!message.member.permissions.has("MANAGE_ROLES"))
             return message.channel.send(
                 "**You need `MANAGE_ROLES` permission to use this command**"
             );
@@ -17,20 +17,26 @@ export const run: RunFunction = async (client, message, args) => {
         if (!emoji)
             return message
                 .reply("**You need use a valid emoji.**")
-                .then((m) => m.delete({ timeout: 1000 }));
+                .then((m) => setTimeout(() => {
+                    m.delete()
+                }, 1000));
 
         const msg = await message.channel.messages.fetch(args[1]);
         if (!msg)
             return message
                 .reply("**Message not found! Wtf...**")
-                .then((m) => m.delete({ timeout: 1000 }));
+                .then((m) => setTimeout(() => {
+                    m.delete()
+                }, 1000));
 
         await client.reactionRoles.deleteReactionRole({
             message: msg,
             emoji,
         });
 
-        message.reply("Done").then((m) => m.delete({ timeout: 500 }));
+        message.reply("Done").then((m) => setTimeout(() => {
+            m.delete()
+        }, 500));
     } catch (err) {
         message.channel.send(
             "**An unexpected Error Occured. Use the correct format: `deleteRR <emoji> <messageID>`**"
