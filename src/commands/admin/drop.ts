d18f6905ebd9754d390de8d87875625812a2d7b0
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 14:29:01 +0600

    😊Drop Done POG

--- src/commands/admin/drop.ts
+++ src/commands/admin/drop.ts
@@ -1,6 +1,6 @@
 import { RunFunction } from '../../interfaces/Command';
 import Nuggies from 'nuggies'
-import { PermissionResolvable } from 'discord.js';
+import { Message, PermissionResolvable } from 'discord.js';
 
 export const name = 'drop'
 export const category = 'admin'
@@ -12,7 +12,7 @@ export const run: RunFunction = async (client, message, args) => {
     let channel
 
     message.channel.send("**What should be the prize of the giveaway. You have 30 seconds to answer this.**")
-    await message.channel.awaitMessages({ max: 1, time: 30000 }).then(collected => {
+    await message.channel.awaitMessages({ filter: (m: Message) => m.author.id === message.author.id, max: 1, time: 30000 }).then(collected => {
         if (collected.first().author.id !== message.author.id) return;
         prize = collected.first()?.content
     }).catch(() => {
@@ -20,7 +20,7 @@ export const run: RunFunction = async (client, message, args) => {
     })
 
     message.channel.send("**What is the channel this giveaway will happen? Type 'here' to do the giveaway here. You have 30 seconds to answer this.**")
-    await message.channel.awaitMessages({ max: 1, time: 30000 }).then(collected => {
+    await message.channel.awaitMessages({ filter: (m: Message) => m.author.id === message.author.id, max: 1, time: 30000 }).then(collected => {
         if (collected.first().author.id !== message.author.id) return;
         if (collected.first()?.content === "here") {
             channel = message.channel.id
