e8009f86c2362f54090a7282ca79fdd85788f393
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 06:31:18 +0600

    🚀Drop Fix

--- src/commands/admin/drop.ts
+++ src/commands/admin/drop.ts
@@ -12,24 +12,24 @@ export const run: RunFunction = async (client, message, args) => {
     let channel
 
     message.channel.send("**What should be the prize of the giveaway. You have 30 seconds to answer this.**")
-    await message.channel.awaitMessages(m => m.author.id == message.author.id,
-        { max: 1, time: 30000 }).then(collected => {
-            prize = collected.first()?.content
-        }).catch(() => {
-            message.reply('**No answer after 30 seconds, operation canceled.**');
-        })
+    await message.channel.awaitMessages({ max: 1, time: 30000 }).then(collected => {
+        if (collected.first().author.id !== message.author.id) return;
+        prize = collected.first()?.content
+    }).catch(() => {
+        message.reply('**No answer after 30 seconds, operation canceled.**');
+    })
 
     message.channel.send("**What is the channel this giveaway will happen? Type 'here' to do the giveaway here. You have 30 seconds to answer this.**")
-    await message.channel.awaitMessages(m => m.author.id == message.author.id,
-        { max: 1, time: 30000 }).then(collected => {
-            if (collected.first()?.content === "here") {
-                channel = message.channel.id
-            } else {
-                channel = collected.first()?.content
-            }
-        }).catch(() => {
-            message.reply('**No answer after 30 seconds, operation canceled.**');
-        })
+    await message.channel.awaitMessages({ max: 1, time: 30000 }).then(collected => {
+        if (collected.first().author.id !== message.author.id) return;
+        if (collected.first()?.content === "here") {
+            channel = message.channel.id
+        } else {
+            channel = collected.first()?.content
+        }
+    }).catch(() => {
+        message.reply('**No answer after 30 seconds, operation canceled.**');
+    })
 
     Nuggies.giveaways.drop({
         message: message,
