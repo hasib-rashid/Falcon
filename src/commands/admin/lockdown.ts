9de91fcb742fd6ae23a797e18380269641ddd1c2
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-28 07:09:06 +0600

    🚀LockDown Fix

--- src/commands/admin/lockdown.ts
+++ src/commands/admin/lockdown.ts
@@ -7,6 +7,11 @@ export const description = 'Lockdown the server'
 export const userPermissions: PermissionResolvable = "MANAGE_CHANNELS"
 
 export const run: RunFunction = async (client, message, args) => {
+    if (!message.member.permissions.has("MANAGE_CHANNELS"))
+        return message.channel.send(
+            "**You need `MANAGE_CHANNELS` permission to use this command**"
+        );
+
     const role = message.guild?.roles.everyone;
 
     const perms = role?.permissions.toArray();
