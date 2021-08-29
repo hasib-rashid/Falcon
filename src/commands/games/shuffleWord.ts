ae8be5eeb79286d496c9ee319139ec44e4f57bde
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-29 22:33:45 +0600

    👑Games Loaded

---
+++ src/commands/games/shuffleWord.ts
@@ -0,0 +1,27 @@
+import { RunFunction } from '../../interfaces/Command';
+import weky from 'weky'
+
+export const name = 'shuffleword'
+export const category = 'games'
+export const description = 'Shuffle Words'
+
+export const run: RunFunction = async (client, message, args) => {
+    await weky.ShuffleGuess({
+        message: message,
+        embed: {
+            title: 'Shuffle Guess | Falcon',
+            color: '#7289da',
+            timestamp: true,
+        },
+        word: ['voice'],
+        button: { cancel: 'Cancel', reshuffle: 'Reshuffle' },
+        startMessage:
+            'I shuffled a word it is **`{{word}}`**. You have **{{time}}** to find the correct word!',
+        winMessage:
+            'GG, It was **{{word}}**! You gave the correct answer in **{{time}}.**',
+        loseMessage: 'Better luck next time! The correct answer was **{{answer}}**.',
+        incorrectMessage: "No {{author}}! The word isn't `{{answer}}`",
+        othersMessage: 'Only <@{{author}}> can use the buttons!',
+        time: 60000,
+    });
+}
