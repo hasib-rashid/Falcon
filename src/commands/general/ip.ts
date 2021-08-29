5a4ec483df0458f44de2cca9a5f86cc805206818
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-29 13:57:44 +0600

    📚Fix

--- src/commands/general/ip.ts
+++ src/commands/general/ip.ts
@@ -1,4 +1,4 @@
-import { RunFunction } from '../../interfaces/Command'; 
+import { RunFunction } from '../../interfaces/Command';
 import { MessageEmbed } from 'discord.js'
 import { default as axios } from 'axios'
 
@@ -7,32 +7,32 @@ export const category = 'general'
 export const description = 'Find the Information about a IP or website'
 
 export const run: RunFunction = async (client, message, args) => {
-     if (!args[0]) return message.channel.send("**Please put a IP or a name of a website.**")
-        axios.get(`http://ip-api.com/json/${args.join(" ")}`).then((response) => {
-            if (response.data.status === "fail") return message.channel.send("**Please Enter a valid IP or a website**")
-            const embed = new MessageEmbed()
-                .setColor("BLUE")
-                .setAuthor(
-                    message.author.username,
-                    message.author.displayAvatarURL()
-                )
-                .setFooter("Powered by IP-API.com")
-                .setTimestamp()
-                .setTitle(args.join(" "))
-                .addField("❯ IP", response.data.query, true)
-                .addField("❯ Country", response.data.country || "None", true)
-                .addField("❯ Region", response.data.regionName || "None", true)
-                .addField(
-                    "❯ City",
-                    response.data.city || response.data.region || "None",
-                    true
-                )
-                .addField("❯ Zip Code", response.data.zip || "None", true)
-                .addField("❯ Timezone", response.data.timezone || "None", true)
-                .addField("❯ ISP", response.data.isp || "None", true)
-                .addField("❯ Organization", response.data.org || "None", true)
-                .addField("❯ Proxy", "Yes", true);
+    if (!args[0]) return message.channel.send("**Please put a IP or a name of a website.**")
+    axios.get(`http://ip-api.com/json/${args.join(" ")}`).then((response) => {
+        if (response.data.status === "fail") return message.channel.send("**Please Enter a valid IP or a website**")
+        const embed = new MessageEmbed()
+            .setColor("BLUE")
+            .setAuthor(
+                message.author.username,
+                message.author.displayAvatarURL()
+            )
+            .setFooter("Powered by IP-API.com")
+            .setTimestamp()
+            .setTitle(args.join(" "))
+            .addField("❯ IP", response.data.query, true)
+            .addField("❯ Country", response.data.country || "None", true)
+            .addField("❯ Region", response.data.regionName || "None", true)
+            .addField(
+                "❯ City",
+                response.data.city || response.data.region || "None",
+                true
+            )
+            .addField("❯ Zip Code", response.data.zip || "None", true)
+            .addField("❯ Timezone", response.data.timezone || "None", true)
+            .addField("❯ ISP", response.data.isp || "None", true)
+            .addField("❯ Organization", response.data.org || "None", true)
+            .addField("❯ Proxy", "Yes", true);
 
-            message.channel.send(embed);
-        })
+        message.channel.send({ embeds: [embed] });
+    })
 }
