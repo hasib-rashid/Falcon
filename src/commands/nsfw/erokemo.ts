3cf6a33762e72a990424af26e4769c4ec74263af
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-29 13:45:24 +0600

    🔞Fix

--- src/commands/nsfw/erokemo.ts
+++ src/commands/nsfw/erokemo.ts
@@ -1,6 +1,7 @@
 import { RunFunction } from '../../interfaces/Command';
 
 import NSFW from 'discord-nsfw'
+import { TextChannel } from 'discord.js';
 const nsfw = new NSFW();
 
 export const name = 'erokemo'
@@ -8,5 +9,6 @@ export const category = 'nsfw'
 export const description = 'NSFW Erokemo'
 
 export const run: RunFunction = async (client, message, args) => {
+    if (!(message.channel as TextChannel).nsfw) return message.channel.send("**You must be in a NSFW Channel to use these commands. 🔞**")
     message.channel.send(await nsfw.erokemo())
 }
