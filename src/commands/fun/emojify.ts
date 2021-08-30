41639441b4d2cbcdce245e853987e5692ee13236
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 23:06:15 +0600

    👑Init

---
+++ src/commands/fun/emojify.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'emojify'
+export const category = 'games'
+export const description = 'Emojify a text'
+
+export const run: RunFunction = async (client, message, args) => {
+
+}
