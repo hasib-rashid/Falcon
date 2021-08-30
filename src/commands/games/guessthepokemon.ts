94ced9dfcefeef7c82519b6d315ea5cdace33145
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 22:59:45 +0600

    ✅Done GTP

--- src/commands/games/guessthepokemon.ts
+++ src/commands/games/guessthepokemon.ts
@@ -5,5 +5,19 @@ export const category = 'games'
 export const description = 'Guess the Pokemon'
 
 export const run: RunFunction = async (client, message, args) => {
+    const { GuessThePokemon } = require('discord-gamecord')
 
+    new GuessThePokemon({
+        message: message,
+        embed: {
+            title: 'Who\'s This Pokemon?',
+            footer: 'You have only 1 chance',
+            color: '#5865F2',
+        },
+        time: 60000,
+        othersMessage: 'You are not allowed to use buttons for this message!',
+        winMessage: 'Your guess was correct! The pokemon was **{pokemon}**',
+        stopMessage: 'Better luck next time! It was a **{pokemon}**',
+        incorrectMessage: 'Your guess was incorrect! The pokemon was **{pokemon}**',
+    }).startGame();
 }
