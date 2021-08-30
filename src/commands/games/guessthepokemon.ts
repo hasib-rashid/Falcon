fafcbbd7457a8acd111d6a549337adc52be7fb63
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 22:59:14 +0600

    ✅Guess the Pokemon

---
+++ src/commands/games/guessthepokemon.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'guessthepokemon'
+export const category = 'games'
+export const description = 'Guess the Pokemon'
+
+export const run: RunFunction = async (client, message, args) => {
+
+}
