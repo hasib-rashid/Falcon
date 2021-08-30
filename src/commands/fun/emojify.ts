d8e8cb1d240f152b93ae0874c369db15ac90b65f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 23:07:19 +0600

    👑Emojify Done lol

--- src/commands/fun/emojify.ts
+++ src/commands/fun/emojify.ts
@@ -5,5 +5,8 @@ export const category = 'games'
 export const description = 'Emojify a text'
 
 export const run: RunFunction = async (client, message, args) => {
+    if (!args.join(" ")) return message.channel.send("**Please provide a text**")
+    const { Emojify } = require('discord-gamecord');
 
+    message.channel.send(await Emojify(args.join(" ")));
 }
