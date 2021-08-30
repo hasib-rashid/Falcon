a44599ffd7fdc90405be0f4a4425d5845a3c664c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 15:55:56 +0600

    🤓Eyy Init

---
+++ src/commands/games/snake.ts
@@ -0,0 +1,10 @@
+import { RunFunction } from '../../interfaces/Command';
+import { Snake } from 'discord-gamecord'
+
+export const name = 'snake'
+export const category = 'games'
+export const description = 'Play the old fashion Snake here'
+
+export const run: RunFunction = async (client, message, args) => {
+
+}
