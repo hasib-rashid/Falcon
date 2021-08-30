c3a2ddd598d159b0399f41ce47bcdcbef1644be6
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 15:56:05 +0600

    🤓SnakeCord Done

--- src/commands/games/snake.ts
+++ src/commands/games/snake.ts
@@ -6,5 +6,22 @@ export const category = 'games'
 export const description = 'Play the old fashion Snake here'
 
 export const run: RunFunction = async (client, message, args) => {
-
+    new Snake({
+        message: message,
+        embed: {
+            title: 'Snake Game',
+            color: '#5865F2',
+            OverTitle: "Game Over",
+        },
+        snake: { head: '🟢', body: '🟩', tail: '🟢' },
+        emojis: {
+            board: '⬛',
+            food: '🍎',
+            up: '⬆️',
+            right: '➡️',
+            down: '⬇️',
+            left: '⬅️',
+        },
+        othersMessage: 'You are not allowed to use buttons for this message!',
+    }).startGame();
 }
