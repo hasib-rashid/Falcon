ed09ccf01f1ae282b0268e9df16569521fc01a6b
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 15:47:46 +0600

    👑RPS Done

--- src/commands/games/rps.ts
+++ src/commands/games/rps.ts
@@ -1,9 +1,5 @@
 import { RunFunction } from '../../interfaces/Command';
-import { MessageEmbed } from 'discord.js';
-import { promptMessage } from "../../util/functions"
-import { User } from 'discord.js';
-
-const chooseArr = ["🗻", "📰", "✂"];
+import { RockPaperScissors } from 'discord-gamecord'
 
 export const name = 'rps'
 export const category = 'games'
@@ -11,20 +7,31 @@ export const description = 'Rock Papper Scissors'
 
 export const run: RunFunction = async (client, message, args) => {
     try {
-
-    } catch {
-        return message.channel.send('**Missing Permissions - [MANAGE_MESSAGES]!**')
-    }
-    function getResult(me: any, botChosen: any) {
-        if ((me === "🗻" && botChosen === "✂") ||
-            (me === "📰" && botChosen === "🗻") ||
-            (me === "✂" && botChosen === "📰")) {
-            return "You won!";
-        } else if (me === botChosen) {
-            return "Its a tie!";
-        } else {
-            return "You lost!";
-        }
-
+        if (!message.mentions.users.first()) return message.channel.send("**Please specify your opponent to play the game**")
+        new RockPaperScissors({
+            message: message,
+            opponent: message.mentions.users.first(),
+            embed: {
+                title: 'Rock Paper Scissors',
+                description: 'Press a button below to make a choice!',
+                color: '#5865F2',
+            },
+            buttons: {
+                rock: 'Rock',
+                paper: 'Paper',
+                scissors: 'Scissors',
+            },
+            othersMessage: 'You are not allowed to use buttons for this message!',
+            chooseMessage: 'You choose {emoji}!',
+            noChangeMessage: 'You cannot change your selection!',
+            askMessage: 'Hey {opponent}, {challenger} challenged you for a game of Rock Paper Scissors!',
+            cancelMessage: 'Looks like they refused to have a game of Rock Paper Scissors. \:(',
+            timeEndMessage: 'Since the opponent didnt answer, i dropped the game!',
+            drawMessage: 'It was a draw!',
+            winMessage: '{winner} won the game!',
+            gameEndMessage: 'The game went unfinished :(',
+        }).startGame();
+    } catch (err) {
+        console.log(err)
     }
 }
