ceaf2aac5df6ee9c84d885b22be5cb04c51c4806
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-30 15:51:31 +0600

    👑Init

---
+++ src/commands/games/connect4.ts
@@ -0,0 +1,9 @@
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'connect4'
+export const category = 'games'
+export const description = 'Play a game of Conect4'
+
+export const run: RunFunction = async (client, message, args) => {
+
+}
