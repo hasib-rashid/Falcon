b94faedb7105d7d3b55ab5df2c9852dbbce261ad
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-31 13:54:47 +0600

    🔧SearchResult

--- src/Base/Client.ts
+++ src/Base/Client.ts
@@ -171,6 +171,9 @@ export default class Falcon extends Client {
 			.on("error", (err) => {
 			})
 			.on("searchNoResult", (message, query) => message.channel.send(`No result found for ${query}!`))
+			.on("searchResult", (message, results) => {
+				message.channel.send(`**Choose an option from below**\n${results.map((song, i) => `**${i + 1}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+			})
 			.on("finish", (queue) =>
 				queue.textChannel.send("**No more song in queue to play. Add More!**")
 			);
