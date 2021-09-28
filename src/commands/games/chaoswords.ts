cbd461e2f57946f161d0837d566d731cfa7ce54c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-09-29 00:11:58 +0600

    📰ChaosWords

---
+++ src/commands/games/chaoswords.ts
@@ -0,0 +1,32 @@
+import { ChaosWords } from 'weky';
+import { RunFunction } from '../../interfaces/Command';
+
+export const name = 'chaoswords'
+export const category = 'games'
+export const description = 'ChaosWords'
+
+export const run: RunFunction = async (client, message, args) => {
+    await ChaosWords({
+        message: message,
+        embed: {
+            title: 'ChaosWords | Falcon',
+            description: '**You have **{{time}}** to find the hidden words in the below sentence.**',
+            color: '#5865F2',
+            field1: '**Sentence: **',
+            field2: '**Words Found/Remaining Words: **',
+            field3: '**Words found: **',
+            field4: '**Words: **',
+            footer: '©️ Falcon',
+            timestamp: true
+        },
+        winMessage: '**GG, You won! You made it in ***{{time}}***.**',
+        loseMessage: '**Better luck next time!**',
+        wrongWordMessage: '**Wrong Guess! You have **{{remaining_tries}}** tries left.**',
+        correctWordMessage: '**GG, ***{{word}}*** was correct! You have to find **{{remaining}}** more word(s).**',
+        time: 60000,
+        charGenerated: 17,
+        maxTries: 10,
+        buttonText: 'Cancel',
+        othersMessage: '**Only <@{{author}}> can use the buttons!**'
+    });
+}
